e92a911d256ae2e57e27bdb7ee9b3d16d65f7538
gguoss <1536310027@qq.com>
2017-07-18 11:00:46 +0800

    chain's log and math library.

---
+++ log/log.go
@@ -0,0 +1,247 @@
+// Package log implements a standard convention for structured logging.
+// Log entries are formatted as K=V pairs.
+// By default, output is written to stdout; this can be changed with SetOutput.
+package log
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"chain/errors"
+)
+
+const rfc3339NanoFixed = "2006-01-02T15:04:05.000000000Z07:00"
+
+// context key type
+type key int
+
+var (
+	logWriterMu sync.Mutex // protects the following
+	logWriter   io.Writer  = os.Stdout
+	procPrefix  []byte     // process-global prefix; see SetPrefix vs AddPrefixkv
+
+	// pairDelims contains a list of characters that may be used as delimeters
+	// between key-value pairs in a log entry. Keys and values will be quoted or
+	// otherwise formatted to ensure that key-value extraction is unambiguous.
+	//
+	// The list of pair delimiters follows Splunk conventions, described here:
+	// http://answers.splunk.com/answers/143368/default-delimiters-for-key-value-extraction.html
+	pairDelims      = " ,;|&\t\n\r"
+	illegalKeyChars = pairDelims + `="`
+
+	// context key for log line prefixes
+	prefixKey key = 0
+)
+
+// Conventional key names for log entries
+const (
+	KeyCaller = "at" // location of caller
+	KeyTime   = "t"  // time of call
+
+	KeyMessage = "message" // produced by Message
+	KeyError   = "error"   // produced by Error
+	KeyStack   = "stack"   // used by Printkv to print stack on subsequent lines
+
+	keyLogError = "log-error" // for errors produced by the log package itself
+)
+
+// SetOutput sets the log output to w.
+// If SetOutput hasn't been called,
+// the default behavior is to write to stdout.
+func SetOutput(w io.Writer) {
+	logWriterMu.Lock()
+	logWriter = w
+	logWriterMu.Unlock()
+}
+
+func appendPrefix(b []byte, keyval ...interface{}) []byte {
+	// Invariant: len(keyval) is always even.
+	if len(keyval)%2 != 0 {
+		panic(fmt.Sprintf("odd-length prefix args: %v", keyval))
+	}
+	for i := 0; i < len(keyval); i += 2 {
+		k := formatKey(keyval[i])
+		v := formatValue(keyval[i+1])
+		b = append(b, k...)
+		b = append(b, '=')
+		b = append(b, v...)
+		b = append(b, ' ')
+	}
+	return b
+}
+
+// SetPrefix sets the global output prefix.
+func SetPrefix(keyval ...interface{}) {
+	b := appendPrefix(nil, keyval...)
+	logWriterMu.Lock()
+	procPrefix = b
+	logWriterMu.Unlock()
+}
+
+// AddPrefixkv appends keyval to any prefix stored in ctx,
+// and returns a new context with the longer prefix.
+func AddPrefixkv(ctx context.Context, keyval ...interface{}) context.Context {
+	p := appendPrefix(prefix(ctx), keyval...)
+	// Note: subsequent calls will append to p, so set cap(p) here.
+	// See TestAddPrefixkvAppendTwice.
+	p = p[0:len(p):len(p)]
+	return context.WithValue(ctx, prefixKey, p)
+}
+
+func prefix(ctx context.Context) []byte {
+	b, _ := ctx.Value(prefixKey).([]byte)
+	return b
+}
+
+// Printkv prints a structured log entry to stdout. Log fields are
+// specified as a variadic sequence of alternating keys and values.
+//
+// Duplicate keys will be preserved.
+//
+// Two fields are automatically added to the log entry: t=[time]
+// and at=[file:line] indicating the location of the caller.
+// Use SkipFunc to prevent helper functions from showing up in the
+// at=[file:line] field.
+//
+// Printkv will also print the stack trace, if any, on separate lines
+// following the message. The stack is obtained from the following,
+// in order of preference:
+//   - a KeyStack value with type []byte or []errors.StackFrame
+//   - a KeyError value with type error, using the result of errors.Stack
+func Printkv(ctx context.Context, keyvals ...interface{}) {
+	// Invariant: len(keyvals) is always even.
+	if len(keyvals)%2 != 0 {
+		keyvals = append(keyvals, "", keyLogError, "odd number of log params")
+	}
+
+	t := time.Now().UTC()
+
+	// Prepend the log entry with auto-generated fields.
+	out := fmt.Sprintf(
+		"%s=%s %s=%s",
+		KeyCaller, caller(),
+		KeyTime, formatValue(t.Format(rfc3339NanoFixed)),
+	)
+
+	var stack interface{}
+	for i := 0; i < len(keyvals); i += 2 {
+		k := keyvals[i]
+		v := keyvals[i+1]
+		if k == KeyStack && isStackVal(v) {
+			stack = v
+			continue
+		}
+		if k == KeyError {
+			if e, ok := v.(error); ok && stack == nil {
+				stack = errors.Stack(errors.Wrap(e)) // wrap to ensure callstack
+			}
+		}
+		out += " " + formatKey(k) + "=" + formatValue(v)
+	}
+
+	logWriterMu.Lock()
+	logWriter.Write(procPrefix)
+	logWriter.Write(prefix(ctx))
+	logWriter.Write([]byte(out)) // ignore errors
+	logWriter.Write([]byte{'\n'})
+	writeRawStack(logWriter, stack)
+	logWriterMu.Unlock()
+}
+
+// Fatalkv is equivalent to Printkv() followed by a call to os.Exit(1).
+func Fatalkv(ctx context.Context, keyvals ...interface{}) {
+	Printkv(ctx, keyvals...)
+	os.Exit(1)
+}
+
+func writeRawStack(w io.Writer, v interface{}) {
+	switch v := v.(type) {
+	case []byte:
+		if len(v) > 0 {
+			w.Write(v)
+			w.Write([]byte{'\n'})
+		}
+	case []errors.StackFrame:
+		for _, s := range v {
+			io.WriteString(w, s.String())
+			w.Write([]byte{'\n'})
+		}
+	}
+}
+
+func isStackVal(v interface{}) bool {
+	switch v.(type) {
+	case []byte:
+		return true
+	case []errors.StackFrame:
+		return true
+	}
+	return false
+}
+
+// Printf prints a log entry containing a message assigned to the
+// "message" key. Arguments are handled as in fmt.Printf.
+func Printf(ctx context.Context, format string, a ...interface{}) {
+	Printkv(ctx, KeyMessage, fmt.Sprintf(format, a...))
+}
+
+// Error prints a log entry containing an error message assigned to the
+// "error" key.
+// Optionally, an error message prefix can be included. Prefix arguments are
+// handled as in fmt.Print.
+func Error(ctx context.Context, err error, a ...interface{}) {
+	if len(a) > 0 && len(errors.Stack(err)) > 0 {
+		err = errors.Wrap(err, a...) // keep err's stack
+	} else if len(a) > 0 {
+		err = fmt.Errorf("%s: %s", fmt.Sprint(a...), err) // don't add a stack here
+	}
+	Printkv(ctx, KeyError, err)
+}
+
+// formatKey ensures that the stringified key is valid for use in a
+// Splunk-style K=V format. It stubs out delimeter and quoter characters in
+// the key string with hyphens.
+func formatKey(k interface{}) string {
+	s := fmt.Sprint(k)
+	if s == "" {
+		return "?"
+	}
+
+	for _, c := range illegalKeyChars {
+		s = strings.Replace(s, string(c), "-", -1)
+	}
+
+	return s
+}
+
+// formatValue ensures that the stringified value is valid for use in a
+// Splunk-style K=V format. It quotes the string value if delimeter or quoter
+// characters are present in the value string.
+func formatValue(v interface{}) string {
+	s := fmt.Sprint(v)
+	if strings.ContainsAny(s, pairDelims) {
+		return strconv.Quote(s)
+	}
+	return s
+}
+
+// RecoverAndLogError must be used inside a defer.
+func RecoverAndLogError(ctx context.Context) {
+	if err := recover(); err != nil {
+		const size = 64 << 10
+		buf := make([]byte, size)
+		buf = buf[:runtime.Stack(buf, false)]
+		Printkv(ctx,
+			KeyMessage, "panic",
+			KeyError, err,
+			KeyStack, buf,
+		)
+	}
+}
---
+++ log/log_test.go
@@ -0,0 +1,365 @@
+package log
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+
+	"chain/errors"
+)
+
+func TestSetOutput(t *testing.T) {
+	var buf bytes.Buffer
+	want := "foobar"
+	SetOutput(&buf)
+	Printf(context.Background(), want)
+	SetOutput(os.Stdout)
+	got := buf.String()
+	if !strings.Contains(got, want) {
+		t.Errorf("log = %q; should contain %q", got, want)
+	}
+}
+
+func TestNoExtraFormatDirectives(t *testing.T) {
+	buf := new(bytes.Buffer)
+	SetOutput(buf)
+	SetPrefix("foo", "bar")
+	Printkv(context.Background(), "baz", 1)
+	SetOutput(os.Stdout)
+	got := buf.String()
+	if strings.Contains(got, "%") {
+		t.Errorf("log line appears to contain format directive: %q", got)
+	}
+}
+
+func TestPrefix(t *testing.T) {
+	buf := new(bytes.Buffer)
+	SetOutput(buf)
+	SetPrefix("foo", "bar")
+	Printkv(context.Background(), "baz", 1)
+	SetOutput(os.Stdout)
+
+	got := buf.String()
+	wantPrefix := "foo=bar "
+	if !strings.HasPrefix(got, wantPrefix) {
+		t.Errorf("output = %q want prefix %q", got, wantPrefix)
+	}
+
+	SetPrefix()
+	if procPrefix != nil {
+		t.Errorf("procPrefix = %q want nil", procPrefix)
+	}
+}
+
+func TestAddPrefixkv0(t *testing.T) {
+	got := prefix(context.Background())
+	var want []byte = nil
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf(`prefix(context.Background()) = %#v want %#v`, got, want)
+	}
+}
+
+func TestAddPrefixkv1(t *testing.T) {
+	ctx := context.Background()
+	ctx1 := AddPrefixkv(ctx, "a", "b")
+	got := prefix(ctx1)
+	want := []byte("a=b ")
+	if !bytes.Equal(got, want) {
+		t.Errorf(`prefix(AddPrefixkv(bg, "a", "b")) = %q want %q`, got, want)
+	}
+}
+
+func TestAddPrefixkv2(t *testing.T) {
+	ctx := context.Background()
+	ctx1 := AddPrefixkv(ctx, "a", "b")
+	ctx2 := AddPrefixkv(ctx1, "c", "d")
+	got := prefix(ctx2)
+	want := []byte("a=b c=d ")
+	if !bytes.Equal(got, want) {
+		t.Errorf(`prefix(AddPrefixkv(AddPrefixkv(bg, "a", "b"), "c", "d")) = %q want %q`, got, want)
+	}
+}
+
+func TestAddPrefixkvAppendTwice(t *testing.T) {
+	ctx := context.Background()
+	ctx1 := AddPrefixkv(ctx, "a", "b")
+	ctx2a := AddPrefixkv(ctx1, "c", "d")
+	ctx2b := AddPrefixkv(ctx1, "e", "f")
+	gota := prefix(ctx2a)
+	wanta := []byte("a=b c=d ")
+	if !bytes.Equal(gota, wanta) {
+		t.Errorf(`prefix(AddPrefixkv(AddPrefixkv(bg, "a", "b"), "c", "d")) = %q want %q`, gota, wanta)
+	}
+	gotb := prefix(ctx2b)
+	wantb := []byte("a=b e=f ")
+	if !bytes.Equal(gotb, wantb) {
+		t.Errorf(`prefix(AddPrefixkv(AddPrefixkv(bg, "a", "b"), "e", "f")) = %q want %q`, gotb, wantb)
+	}
+}
+
+func TestPrintkv(t *testing.T) {
+	examples := []struct {
+		keyvals []interface{}
+		want    []string
+	}{
+		// Basic example
+		{
+			keyvals: []interface{}{"msg", "hello world"},
+			want: []string{
+				"at=log_test.go:",
+				"t=",
+				`msg="hello world"`,
+			},
+		},
+
+		// Duplicate keys
+		{
+			keyvals: []interface{}{"msg", "hello world", "msg", "goodbye world"},
+			want: []string{
+				"at=log_test.go:",
+				"t=",
+				`msg="hello world"`,
+				`msg="goodbye world"`,
+			},
+		},
+
+		// Zero log params
+		{
+			keyvals: nil,
+			want: []string{
+				"at=log_test.go:",
+				"t=",
+			},
+		},
+
+		// Odd number of log params
+		{
+			keyvals: []interface{}{"k1", "v1", "k2"},
+			want: []string{
+				"at=log_test.go:",
+				"t=",
+				"k1=v1",
+				"k2=",
+				`log-error="odd number of log params"`,
+			},
+		},
+	}
+
+	for i, ex := range examples {
+		t.Log("Example", i)
+
+		buf := new(bytes.Buffer)
+		SetOutput(buf)
+
+		Printkv(context.Background(), ex.keyvals...)
+
+		read, err := ioutil.ReadAll(buf)
+		if err != nil {
+			SetOutput(os.Stdout)
+			t.Fatal("read buffer error:", err)
+		}
+
+		got := string(read)
+
+		for _, w := range ex.want {
+			if !strings.Contains(got, w) {
+				t.Errorf("Result did not contain string:\ngot:  %s\nwant: %s", got, w)
+			}
+		}
+		if !strings.HasSuffix(got, "\n") {
+			t.Errorf("log output should end with a newline")
+		}
+
+		SetOutput(os.Stdout)
+	}
+}
+
+func TestMessagef(t *testing.T) {
+	buf := new(bytes.Buffer)
+	SetOutput(buf)
+	defer SetOutput(os.Stdout)
+
+	Printf(context.Background(), "test round %d", 0)
+
+	read, err := ioutil.ReadAll(buf)
+	if err != nil {
+		t.Fatal("read buffer error:", err)
+	}
+
+	got := string(read)
+	want := []string{
+		"at=log_test.go:",
+		`message="test round 0"`,
+	}
+
+	for _, w := range want {
+		if !strings.Contains(got, w) {
+			t.Errorf("Result did not contain string:\ngot:  %s\nwant: %s", got, w)
+		}
+	}
+}
+
+func TestPrintkvStack(t *testing.T) {
+	buf := new(bytes.Buffer)
+	SetOutput(buf)
+	defer SetOutput(os.Stdout)
+
+	root := errors.New("boo")
+	wrapped := errors.Wrap(root)
+	Printkv(context.Background(), KeyError, wrapped)
+
+	read, err := ioutil.ReadAll(buf)
+	if err != nil {
+		t.Fatal("read buffer error:", err)
+	}
+
+	got := string(read)
+	want := []string{
+		"at=log_test.go:",
+		"error=boo",
+
+		// stack trace
+		"TestPrintkvStack\n",
+		"/go/",
+	}
+
+	t.Logf("output:\n%s", got)
+	for _, w := range want {
+		if !strings.Contains(got, w) {
+			t.Errorf("output %q did not contain %q", got, w)
+		}
+	}
+}
+
+func TestError(t *testing.T) {
+	buf := new(bytes.Buffer)
+	SetOutput(buf)
+	defer SetOutput(os.Stdout)
+
+	root := errors.New("boo")
+	wrapped := errors.Wrap(root)
+	Error(context.Background(), wrapped, "failure x ", 0)
+
+	read, err := ioutil.ReadAll(buf)
+	if err != nil {
+		t.Fatal("read buffer error:", err)
+	}
+
+	got := string(read)
+	want := []string{
+		"at=log_test.go:",
+		`error="failure x 0: boo"`,
+
+		// stack trace
+		"TestError\n",
+		"/go/",
+	}
+
+	t.Logf("output:\n%s", got)
+	for _, w := range want {
+		if !strings.Contains(got, w) {
+			t.Errorf("output %q did not contain %q", got, w)
+		}
+	}
+}
+
+func TestRawStack(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	defer SetOutput(os.Stdout)
+
+	stack := []byte("this\nis\na\nraw\nstack")
+	Printkv(context.Background(), "message", "foo", "stack", stack)
+
+	got := buf.String()
+	if !strings.HasSuffix(got, "\n"+string(stack)+"\n") {
+		t.Logf("output:\n%s", got)
+		t.Errorf("output did not contain %q", stack)
+	}
+}
+
+func TestIsStackVal(t *testing.T) {
+	cases := []struct {
+		v interface{}
+		w bool
+	}{
+		{[]byte("foo"), true},
+		{[]errors.StackFrame{}, true},
+		{"line1", false},
+		{[...]byte{'x'}, false},
+		{[]string{}, false},
+	}
+	for _, test := range cases {
+		if g := isStackVal(test.v); g != test.w {
+			t.Errorf("isStackVal(%#v) = %v want %v", test.v, g, test.w)
+		}
+	}
+}
+
+func TestWriteRawStack(t *testing.T) {
+	cases := []struct {
+		v interface{}
+		w string
+	}{
+		{[]byte("foo\nbar"), "foo\nbar\n"},
+		{[]errors.StackFrame{{Func: "foo", File: "f.go", Line: 1}}, "f.go:1 - foo\n"},
+		{1, ""}, // int is not a valid stack val
+	}
+
+	for _, test := range cases {
+		var buf bytes.Buffer
+		writeRawStack(&buf, test.v)
+		if g := buf.String(); g != test.w {
+			t.Errorf("writeRawStack(%#v) = %q want %q", test.v, g, test.w)
+		}
+	}
+}
+
+func TestFormatKey(t *testing.T) {
+	examples := []struct {
+		key  interface{}
+		want string
+	}{
+		{"hello", "hello"},
+		{"hello world", "hello-world"},
+		{"", "?"},
+		{true, "true"},
+		{"a b\"c\nd;e\tf龜g", "a-b-c-d-e-f龜g"},
+	}
+
+	for i, ex := range examples {
+		t.Log("Example", i)
+		got := formatKey(ex.key)
+		if got != ex.want {
+			t.Errorf("formatKey(%#v) = %q want %q", ex.key, got, ex.want)
+		}
+	}
+}
+
+func TestFormatValue(t *testing.T) {
+	examples := []struct {
+		value interface{}
+		want  string
+	}{
+		{"hello", "hello"},
+		{"hello world", `"hello world"`},
+		{1.5, "1.5"},
+		{true, "true"},
+		{errors.New("this is an error"), `"this is an error"`},
+		{[]byte{'a', 'b', 'c'}, `"[97 98 99]"`},
+		{bytes.NewBuffer([]byte{'a', 'b', 'c'}), "abc"},
+		{"a b\"c\nd;e\tf龜g", `"a b\"c\nd;e\tf龜g"`},
+	}
+
+	for i, ex := range examples {
+		t.Log("Example", i)
+		got := formatValue(ex.value)
+		if got != ex.want {
+			t.Errorf("formatKey(%#v) = %q want %q", ex.value, got, ex.want)
+		}
+	}
+}
