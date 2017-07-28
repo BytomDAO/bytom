d0a6e521903ef36e666d0a060cd2143bd7b9f420
gguoss <1536310027@qq.com>
2017-07-28 16:05:54 +0800

    Delete vendor directory, remove dependecy.

--- vendor/github.com/go-kit/kit/sd/eureka/doc.go
+++
@@ -1,2 +0,0 @@
-// Package eureka provides subscriber and registrar implementations for Netflix OSS's Eureka
-package eureka
--- vendor/github.com/tendermint/merkleeyes/iavl/version.go
+++
@@ -1,3 +0,0 @@
-package iavl
-
-const Version = "0.4.0" // benchmarking, update proof
