be991aab63a75d08228982d1f289dc119179be1a
gguoss <1536310027@qq.com>
2017-09-08 14:17:03 +0800

    Added example about build-transaction.

--- cmd/bytomcli/example/issue.go
+++ cmd/bytomcli/example/issue.go
@@ -12,7 +12,7 @@ import (
 	//"path/filepath"
 	//"strings"
 	//"time"
-	//stdjson "encoding/json"
+	stdjson "encoding/json"
 
 	//"github.com/bytom/blockchain"
 	"github.com/bytom/blockchain/rpc"
@@ -22,6 +22,8 @@ import (
 	//"github.com/bytom/log"
 	"github.com/bytom/crypto/ed25519/chainkd"
 	"github.com/bytom/blockchain/query"
+	"github.com/bytom/blockchain/txbuilder"
+	bc "github.com/bytom/blockchain"
 )
 
 // TO DO: issue a asset to a account.
@@ -45,7 +47,7 @@ func IssueTest(client *rpc.Client, args []string) {
 	ins.Alias = "alice"
 	ins.Tags = map[string]interface{}{"test_tag": "v0",}
 	ins.ClientToken = "account"
-	account := make([]query.AnnotatedAccount, 2)
+	account := make([]query.AnnotatedAccount, 1)
 	client.Call(context.Background(), "/create-account", &[]Ins{ins,}, &account)
 	fmt.Printf("account:%v\n", account)
 
@@ -75,12 +77,22 @@ func IssueTest(client *rpc.Client, args []string) {
 	client.Call(context.Background(), "/create-asset", &[]Ins_asset{ins_asset,}, &asset)
 	fmt.Printf("asset:%v\n", asset)
 
-/*
-	for i := range account {
-		var account_ query.AnnotatedAccount
-		//account_ = account[i].(query.AnnotatedAccount)
-		//stdjson.Unmarshal(account[i].([]byte), &account_)
-		fmt.Printf("account:%v\n", account[i][0])
+
+	// Build Transaction.
+	fmt.Printf("To build transaction:\n")
+	// Now Issue actions
+	buildReqFmt := `
+		{"actions": [
+			{"type": "issue", "asset_id": "%s", "amount": 100},
+			{"type": "control_account", "asset_id": "%s", "amount": 100, "account_id": "%s"}
+		]}`
+	buildReqStr := fmt.Sprintf(buildReqFmt, asset[0].ID.String(), asset[0].ID.String(), account[0].ID)
+	var buildReq bc.BuildRequest
+	err := stdjson.Unmarshal([]byte(buildReqStr), &buildReq)
+	if err != nil {
+		fmt.Printf("json Unmarshal error.")
 	}
-	*/
+	tpl := make([]txbuilder.Template, 1)
+	client.Call(context.Background(), "/build-transaction", []*bc.BuildRequest{&buildReq,}, &tpl)
+	fmt.Printf("tpl:%v\n", tpl)
 }
