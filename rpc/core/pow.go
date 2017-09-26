3d9016bd7c7ee388ac92016dd3c07c97847675a0
gguoss <1536310027@qq.com>
2017-09-26 14:10:08 +0800

    Add a rpc: getBlockHeight

--- rpc/core/pow.go
+++ rpc/core/pow.go
@@ -1,26 +1,14 @@
 package core
 
 import (
-        //"fmt"
-        //"context"
-        //"github.com/blockchain/protocol/bc"
         ctypes "github.com/bytom/rpc/core/types"
-        //"github.com/blockchain/protocol"
-        //"chain/protocol/bc/legacy" 
-        //"github.com/consensus/types"
-        //. "github.com/tendermint/tmlibs/common"sour	
         "github.com/bytom/protocol/bc/legacy"
 )
 
-//for simulate
 func GetWork()(*ctypes.ResultBlockHeaderInfo, error){
-    //ctx := context.Background()
-    //b1 := &legacy.Block{BlockHeader: legacy.BlockHeader{Height: 0}}
     return &ctypes.ResultBlockHeaderInfo{},nil
 }
 
-
-//func SubmitWork(blkheader ctypes.ResultBlockHeaderInfo) (bool,error) {
 func SubmitWork(height uint64) (bool,error) {
     block := legacy.Block{
                 BlockHeader: legacy.BlockHeader{
