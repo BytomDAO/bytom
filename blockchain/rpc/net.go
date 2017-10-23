b8520d19b31a0022ced958550fa1f60c76ab1530
gguoss <1536310027@qq.com>
2017-10-23 14:43:25 +0800

    Add rpc about network status

---
+++ blockchain/rpc/net.go
@@ -0,0 +1,46 @@
+package rpc
+
+import (
+	"github.com/bytom/p2p"
+	ctypes "github.com/bytom/blockchain/rpc/types"
+)
+
+//-----------------------------------------------------------------------------
+
+func NetInfo(p2pSwitch *p2p.Switch) (*ctypes.ResultNetInfo, error) {
+	listening := p2pSwitch.IsListening()
+	listeners := []string{}
+	for _, listener := range p2pSwitch.Listeners() {
+		listeners = append(listeners, listener.String())
+	}
+	peers := []ctypes.Peer{}
+	for _, peer := range p2pSwitch.Peers().List() {
+		peers = append(peers, ctypes.Peer{
+			NodeInfo:         *peer.NodeInfo,
+			IsOutbound:       peer.IsOutbound(),
+			ConnectionStatus: peer.Connection().Status(),
+		})
+	}
+	return &ctypes.ResultNetInfo{
+		Listening: listening,
+		Listeners: listeners,
+		Peers:     peers,
+	}, nil
+}
+
+//-----------------------------------------------------------------------------
+
+// Dial given list of seeds
+/*func UnsafeDialSeeds(seeds []string) (*ctypes.ResultDialSeeds, error) {
+
+	if len(seeds) == 0 {
+		return &ctypes.ResultDialSeeds{}, fmt.Errorf("No seeds provided")
+	}
+	// starts go routines to dial each seed after random delays
+	logger.Info("DialSeeds", "addrBook", addrBook, "seeds", seeds)
+	err := p2pSwitch.DialSeeds(addrBook, seeds)
+	if err != nil {
+		return &ctypes.ResultDialSeeds{}, err
+	}
+	return &ctypes.ResultDialSeeds{"Dialing seeds in progress. See /net_info for details"}, nil
+}*/
