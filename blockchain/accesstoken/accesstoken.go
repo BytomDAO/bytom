336c396f7e0d517f314507ab679417e9fee16c80
yahtoo <yahtoo.ma@gmail.com>
2017-11-17 15:30:37 +0800

    add access token function (#111)
    * add access token function
    
    * add access token function
    
    * add access token function

--- blockchain/accesstoken/accesstoken.go
+++ blockchain/accesstoken/accesstoken.go
@@ -2,23 +2,22 @@
 // credentials.
 package accesstoken
 
-// TODO(tessr): merge this package into chain/net/http/authn
-
 import (
 	"context"
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"regexp"
+	"strings"
 	"time"
+
+	dbm "github.com/tendermint/tmlibs/db"
 
 	"github.com/bytom/crypto/sha3pool"
 	"github.com/bytom/errors"
 )
 
-const (
-	tokenSize    = 32
-	defaultLimit = 100
-)
+const tokenSize = 32
 
 var (
 	// ErrBadID is returned when Create is called on an invalid id string.
@@ -27,147 +26,132 @@ var (
 	ErrDuplicateID = errors.New("duplicate access token ID")
 	// ErrBadType is returned when Create is called with a bad type.
 	ErrBadType = errors.New("type must be client or network")
+	// ErrNoMatchID is returned when Delete is called on nonexisting ID.
+	ErrNoMatchID = errors.New("nonexisting access token ID")
 
 	// validIDRegexp checks that all characters are alphumeric, _ or -.
 	// It also must have a length of at least 1.
 	validIDRegexp = regexp.MustCompile(`^[\w-]+$`)
 )
 
+// Token describe the access token.
 type Token struct {
 	ID      string    `json:"id"`
 	Token   string    `json:"token,omitempty"`
-	Type    string    `json:"type,omitempty"` // deprecated in 1.2
+	Type    string    `json:"type,omitempty"`
 	Created time.Time `json:"created_at"`
-	sortID  string
 }
 
+// CredentialStore store user access credential.
 type CredentialStore struct {
-	//	DB pg.DB
+	DB dbm.DB
+}
+
+// NewStore creates and returns a new Store object.
+func NewStore(db dbm.DB) *CredentialStore {
+	return &CredentialStore{
+		DB: db,
+	}
 }
 
 // Create generates a new access token with the given ID.
-func (cs *Token) Create(ctx context.Context, id, typ string) (*Token, error) {
+func (cs *CredentialStore) Create(ctx context.Context, id, typ string) (*string, error) {
 	if !validIDRegexp.MatchString(id) {
 		return nil, errors.WithDetailf(ErrBadID, "invalid id %q", id)
 	}
-
+	k, err := json.Marshal(id)
+	if v := cs.DB.Get(k); v != nil {
+		return nil, errors.WithDetailf(ErrDuplicateID, "id %q already in use", id)
+	}
 	var secret [tokenSize]byte
-	_, err := rand.Read(secret[:])
+	v, err := rand.Read(secret[:])
+	if err != nil || v != tokenSize {
+		return nil, err
+	}
+
+	var hashedSecret [tokenSize]byte
+	sha3pool.Sum256(hashedSecret[:], secret[:])
+	created := time.Now()
+
+	token := &Token{
+		ID:      id,
+		Token:   fmt.Sprintf("%s:%x", id, hashedSecret),
+		Type:    typ,
+		Created: created,
+	}
+
+	key, err := json.Marshal(id)
 	if err != nil {
 		return nil, err
 	}
-	var hashedSecret [32]byte
-	sha3pool.Sum256(hashedSecret[:], secret[:])
-
-	/*	const q = `
-			INSERT INTO access_tokens (id, type, hashed_secret)
-			VALUES($1, $2, $3)
-			RETURNING created, sort_id
-		`
-	*/var (
-		created time.Time
-		sortID  string
-	//	maybeType = sql.NullString{String: typ, Valid: typ != ""}
-	)
-	/*	err = cs.DB.QueryRowContext(ctx, q, id, maybeType, hashedSecret[:]).Scan(&created, &sortID)
-		if pg.IsUniqueViolation(err) {
-			return nil, errors.WithDetailf(ErrDuplicateID, "id %q already in use", id)
-		}
-		if err != nil {
-			return nil, errors.Wrap(err)
-		}
-	*/
-	return &Token{
-		ID:      id,
-		Token:   fmt.Sprintf("%s:%x", id, secret),
-		Type:    typ,
-		Created: created,
-		sortID:  sortID,
-	}, nil
+	value, err := json.Marshal(token)
+	if err != nil {
+		return nil, err
+	}
+	cs.DB.Set(key, value)
+	hexsec := fmt.Sprintf("%s:%x", id, secret)
+	return &hexsec, nil
 }
 
 // Check returns whether or not an id-secret pair is a valid access token.
-func (cs *Token) Check(ctx context.Context, id string, secret []byte) (bool, error) {
-	var (
-		toHash [tokenSize]byte
-		hashed [32]byte
-	)
+func (cs *CredentialStore) Check(ctx context.Context, id string, secret []byte) (bool, error) {
+	if !validIDRegexp.MatchString(id) {
+		return false, errors.WithDetailf(ErrBadID, "invalid id %q", id)
+	}
+
+	var toHash [tokenSize]byte
+	var hashed [tokenSize]byte
 	copy(toHash[:], secret)
 	sha3pool.Sum256(hashed[:], toHash[:])
+	inToken := fmt.Sprintf("%s:%x", id, hashed[:])
 
-	/*const q = `SELECT EXISTS(SELECT 1 FROM access_tokens WHERE id=$1 AND hashed_secret=$2)`
-	var valid bool
-	err := cs.DB.QueryRowContext(ctx, q, id, hashed[:]).Scan(&valid)
+	var value []byte
+	token := &Token{}
+	k, err := json.Marshal(id)
 	if err != nil {
 		return false, err
 	}
-	*/
-	//	return valid, nil
-	return true, nil
-}
 
-// Exists returns whether an id is part of a valid access token. It does not validate a secret.
-func (cs *Token) Exists(ctx context.Context, id string) bool {
-	/*	const q = `SELECT EXISTS(SELECT 1 FROM access_tokens WHERE id=$1)`
-		var valid bool
-		err := cs.DB.QueryRowContext(ctx, q, id).Scan(&valid)
-		if err != nil {
-			return false
-		}
-		return valid
-	*/
-	return true
+	if value = cs.DB.Get(k); value == nil {
+		return false, errors.WithDetailf(ErrNoMatchID, "check id %q nonexisting", id)
+	}
+	if err := json.Unmarshal(value, token); err != nil {
+		return false, err
+	}
+
+	if strings.Compare(token.Token, inToken) == 0 {
+		return true, nil
+	}
+
+	return false, nil
 }
 
 // List lists all access tokens.
-func (cs *Token) List(ctx context.Context, typ, after string, limit int) ([]*Token, string, error) {
-	if limit == 0 {
-		limit = defaultLimit
-	}
-	/*	const q = `
-				SELECT id, type, sort_id, created FROM access_tokens
-				WHERE ($1='' OR type=$1::access_token_type) AND ($2='' OR sort_id<$2)
-				ORDER BY sort_id DESC
-				LIMIT $3
-			`
-		var tokens []*Token
-		//	err := pg.ForQueryRows(ctx, cs.DB, q, typ, after, limit, func(id string, maybeType sql.NullString, sortID string, created time.Time) {
-		t := Token{
-			ID:      id,
-			Created: created,
-			Type:    maybeType.String,
-			sortID:  sortID,
-		}
-		tokens = append(tokens, &t)
-			})
-		if err != nil {
-			return nil, "", errors.Wrap(err)
-		}
-	*/
-	var tokens []*Token
-	var next string
-	if len(tokens) > 0 {
-		next = tokens[len(tokens)-1].sortID
-	}
+func (cs *CredentialStore) List(ctx context.Context) ([]*Token, error) {
+	tokens := make([]*Token, 0)
+	iter := cs.DB.Iterator()
+	defer iter.Release()
 
-	return tokens, next, nil
+	for iter.Next() {
+		token := &Token{}
+		if err := json.Unmarshal(iter.Value(), token); err != nil {
+			return nil, err
+		}
+		tokens = append(tokens, token)
+	}
+	return tokens, nil
 }
 
 // Delete deletes an access token by id.
-func (cs *Token) Delete(ctx context.Context, id string) error {
-	/*	const q = `DELETE FROM access_tokens WHERE id=$1`
-		res, err := cs.DB.ExecContext(ctx, q, id)
-		if err != nil {
-			return errors.Wrap(err)
-		}
-	*/
-	/*	deleted, err := res.RowsAffected()
-		if err != nil {
-			return errors.Wrap(err)
-		}
+func (cs *CredentialStore) Delete(ctx context.Context, id string) error {
+	if !validIDRegexp.MatchString(id) {
+		return errors.WithDetailf(ErrBadID, "invalid id %q", id)
+	}
+	k, err := json.Marshal(id)
+	if err != nil {
+		return err
+	}
+	cs.DB.Delete(k)
 
-		if deleted == 0 {
-			return errors.WithDetailf(pg.ErrUserInputNotFound, "acccess token id %s", id)
-		}
-	*/return nil
+	return nil
 }
