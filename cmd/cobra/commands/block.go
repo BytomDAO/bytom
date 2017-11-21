b80ce6da6fff6c2dd9dc6e1847b762372d8e8d50
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2017-11-21 18:47:54 +0800

    Block height get block tx cnt by height (#121)
    * Add get-block-transactions-by-height rpc
    
    * Add block-height
    
    * Simplify get-best-block-hash

--- cmd/cobra/commands/block.go
+++ cmd/cobra/commands/block.go
@@ -16,14 +16,25 @@ var (
 	coreURL = env.String("BYTOM_URL", "http://localhost:1999")
 )
 
-var getBestBlockHashCmd = &cobra.Command{
-	Use:   "get-best-block-hash",
-	Short: "Get the most recent block hash",
+var blockHashCmd = &cobra.Command{
+	Use:   "block-hash",
+	Short: "Get the hash of most recent block",
 	Run: func(cmd *cobra.Command, args []string) {
 		var response interface{}
 		client := mustRPCClient()
 		client.Call(context.Background(), "/get-best-block-hash", nil, &response)
 		jww.FEEDBACK.Printf("best block hash: %v\n", response)
+	},
+}
+
+var blockHeightCmd = &cobra.Command{
+	Use:   "block-height",
+	Short: "Get the number of most recent block",
+	Run: func(cmd *cobra.Command, args []string) {
+		var response interface{}
+		client := mustRPCClient()
+		client.Call(context.Background(), "/block-height", nil, &response)
+		jww.FEEDBACK.Printf("block height: %v\n", response)
 	},
 }
 
@@ -92,5 +103,26 @@ var getBlockByHeightCmd = &cobra.Command{
 		client := mustRPCClient()
 		client.Call(context.Background(), "/get-block-by-height", ui64, &response)
 		jww.FEEDBACK.Printf("%v\n", response)
+	},
+}
+
+var getBlockTransactionsCountByHeightCmd = &cobra.Command{
+	Use:   "get-block-transactions-count-by-height",
+	Short: "Get the transactions count of a block matching the given height",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			jww.ERROR.Println("get-block-header-by-height args not valid\nUsage: get-block-transactions-count-by-height [height]")
+			return
+		}
+
+		ui64, err := strconv.ParseUint(args[0], 10, 64)
+		if err != nil {
+			jww.ERROR.Printf("Invalid height value")
+			return
+		}
+		var response interface{}
+		client := mustRPCClient()
+		client.Call(context.Background(), "/get-block-transactions-count-by-height", ui64, &response)
+		jww.FEEDBACK.Printf("transactions count: %v\n", response)
 	},
 }
