a18831d55a34bad261b30fc449001cacf31cfb34
Chengcheng Zhang <zcc0721@foxmail.com>
2017-11-22 16:00:29 +0800

    udpate base.go

--- consensus/algorithm/base.go
+++ consensus/algorithm/base.go
@@ -2,14 +2,14 @@ package algorithm
 
 import "github.com/bytom/protocol/bc"
 
-func CreateCache(seed *bc.Hash) ([]uint32, error) {
+func MockCreateCache(seed *bc.Hash) ([]uint32, error) {
 	return []uint32{}, nil
 }
 
-func CreateSeed(height uint64, preSeed *bc.Hash, preBlockHashs []*bc.Hash) *bc.Hash {
+func MockCreateSeed(height uint64, preSeed *bc.Hash, preBlockHashs []*bc.Hash) *bc.Hash {
 	return preBlockHashs[0]
 }
 
-func AIHash(height uint64, blockHash *bc.Hash, seedCache []uint32) (*bc.Hash, error) {
+func MockAIHash(height uint64, blockHash *bc.Hash, seedCache []uint32) (*bc.Hash, error) {
 	return blockHash, nil
 }
