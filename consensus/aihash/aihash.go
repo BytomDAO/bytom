cd4bb5597cc05100ddc0517fd80f43763e73f50e
Chengcheng Zhang <zcc0721@foxmail.com>
2017-11-27 16:35:20 +0800

    update code review

--- consensus/aihash/aihash.go
+++ consensus/aihash/aihash.go
@@ -10,7 +10,7 @@ import (
 
 // CreateSeed return epoch seed, type is *bc.Hash
 func CreateSeed(height uint64, preSeed *bc.Hash, preEpochBlockHash []*bc.Hash) *bc.Hash {
-	if (height-1)%epochLength != 0 || height <= epochLength {
+	if (height-1)%epochLength != 0 {
 		log.WithFields(log.Fields{
 			"height": height,
 			"epoch":  (height - 1) / epochLength,
@@ -24,21 +24,19 @@ func CreateSeed(height uint64, preSeed *bc.Hash, preEpochBlockHash []*bc.Hash) *
 		"height": height,
 		"epoch":  (height - 1) / epochLength,
 		"seed":   seed.String(),
-	}).Info("Created new seed.")
+	}).Debug("Created new seed.")
 
 	return seed
 }
 
 // CreateCache return cache, type is []int32
 func CreateCache(seed *bc.Hash) ([]uint32, error) {
-	log.Info("Start creating cache...")
+	log.Debug("Start creating cache...")
 	if seed == nil {
 		return nil, errors.New("Seed is invalid or not exist!")
 	}
 	cache := make([]uint32, cacheLength/4)
 	generateCache(cache, (*seed).Bytes())
-
-	log.Info("Create cache completely.")
 
 	return cache, nil
 }
