3c287f93b2a18872a5fb82b445196d5c3db975c2
Yahtoo Ma <yahtoo.ma@gmail.com>
2017-11-28 19:10:38 +0800

    Change accesstoken handler  from  "a" to "br"

--- blockchain/accesstokens.go
+++ blockchain/accesstokens.go
@@ -9,8 +9,8 @@ import (
 
 var errCurrentToken = errors.New("token cannot delete itself")
 
-func (a *BlockchainReactor) createAccessToken(ctx context.Context, x struct{ ID, Type string }) interface{} {
-	token, err := a.accessTokens.Create(ctx, x.ID, x.Type)
+func (br *BlockchainReactor) createAccessToken(ctx context.Context, x struct{ ID, Type string }) interface{} {
+	token, err := br.accessTokens.Create(ctx, x.ID, x.Type)
 	if err != nil {
 		return jsendWrapper(nil, ERROR, err.Error())
 	}
@@ -18,8 +18,8 @@ func (a *BlockchainReactor) createAccessToken(ctx context.Context, x struct{ ID,
 	return jsendWrapper(token, SUCCESS, "")
 }
 
-func (a *BlockchainReactor) listAccessTokens(ctx context.Context) interface{} {
-	tokens, err := a.accessTokens.List(ctx)
+func (br *BlockchainReactor) listAccessTokens(ctx context.Context) interface{} {
+	tokens, err := br.accessTokens.List(ctx)
 	if err != nil {
 		return jsendWrapper(nil, ERROR, err.Error())
 	}
@@ -27,20 +27,20 @@ func (a *BlockchainReactor) listAccessTokens(ctx context.Context) interface{} {
 	return jsendWrapper(tokens, SUCCESS, "")
 }
 
-func (a *BlockchainReactor) deleteAccessToken(ctx context.Context, x struct{ ID, Token string }) interface{} {
+func (br *BlockchainReactor) deleteAccessToken(ctx context.Context, x struct{ ID, Token string }) interface{} {
 	//TODO Add delete permission verify.
-	if err := a.accessTokens.Delete(ctx, x.ID); err != nil {
+	if err := br.accessTokens.Delete(ctx, x.ID); err != nil {
 		return jsendWrapper(nil, ERROR, err.Error())
 	}
 	return jsendWrapper("success", SUCCESS, "")
 }
 
-func (a *BlockchainReactor) checkAccessToken(ctx context.Context, x struct{ ID, Secret string }) interface{} {
+func (br *BlockchainReactor) checkAccessToken(ctx context.Context, x struct{ ID, Secret string }) interface{} {
 	secret, err := hex.DecodeString(x.Secret)
 	if err != nil {
 		return jsendWrapper(nil, ERROR, err.Error())
 	}
-	result, err := a.accessTokens.Check(ctx, x.ID, secret)
+	result, err := br.accessTokens.Check(ctx, x.ID, secret)
 	if err != nil {
 		return jsendWrapper(nil, ERROR, err.Error())
 	}
