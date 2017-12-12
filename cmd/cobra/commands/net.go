bf8da2d8912a4a6a63ffb467c991fef38f4688ca
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2017-12-12 09:38:37 +0800

    Add exit code
    - add exit code for various exceptions.

--- cmd/cobra/commands/net.go
+++ cmd/cobra/commands/net.go
@@ -22,7 +22,7 @@ var netListeningCmd = &cobra.Command{
 	Use:   "net-listening",
 	Short: "If client is actively listening for network connections",
 	Run: func(cmd *cobra.Command, args []string) {
-		if data := clientCall("/net-listening", nil); data != nil {
+		if data := clientCall("/net-listening"); data != nil {
 			res, err := strconv.ParseBool(data[0])
 			if err != nil {
 				jww.ERROR.Println("Fail to parse response data")
@@ -37,7 +37,7 @@ var peerCountCmd = &cobra.Command{
 	Use:   "peer-count",
 	Short: "Number of peers currently connected to the client",
 	Run: func(cmd *cobra.Command, args []string) {
-		if data := clientCall("/peer-count", nil); data != nil {
+		if data := clientCall("/peer-count"); data != nil {
 			i, err := strconv.ParseInt(data[0], 16, 64)
 			if err != nil {
 				jww.ERROR.Println("Fail to parse response data")
@@ -52,7 +52,7 @@ var netSyncingCmd = &cobra.Command{
 	Use:   "net-syncing",
 	Short: "If the network is still syncing",
 	Run: func(cmd *cobra.Command, args []string) {
-		if data := clientCall("/net-syncing", nil); data != nil {
+		if data := clientCall("/net-syncing"); data != nil {
 			res, err := strconv.ParseBool(data[0])
 			if err != nil {
 				jww.ERROR.Println("Fail to parse response data")
