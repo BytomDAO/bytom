515e877475a4bc329b42c816aa511b469638e791
gguoss <1536310027@qq.com>
2017-12-25 17:14:41 +0800

    Add curl test

---
+++ test/integration/bash_rpc/create-account.sh
@@ -0,0 +1,1 @@
+curl -d '[{"root_xpubs":["2d6c07cb1ff7800b0793e300cd62b6ec5c0943d308799427615be451ef09c0304bee5dd492c6b13aaa854d303dc4f1dcb229f9578786e19c52d860803efa3b9a"],"Quorum":1,"Alias":"aa","Tags":{"test_tag":"v0"},"client_token":"test_token"}]' -X POST http://localhost:9888/create-account
