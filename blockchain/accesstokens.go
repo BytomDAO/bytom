1c1372d193dbe56a165179bfce5ebf8c360d2d15
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2017-12-29 16:04:13 +0800

    Revert accesstokens.go

--- blockchain/accesstokens.go
+++ blockchain/accesstokens.go
@@ -4,58 +4,48 @@ import (
 	"context"
 	"encoding/hex"
 
-	log "github.com/sirupsen/logrus"
-
 	"github.com/bytom/errors"
 )
 
 var errCurrentToken = errors.New("token cannot delete itself")
 
-func (br *BlockchainReactor) createAccessToken(ctx context.Context, x struct {
-	ID   string `json:"id"`
-	Type string `json:"type"`
-}) Response {
+func (br *BlockchainReactor) createAccessToken(ctx context.Context, x struct{ ID, Type string }) interface{} {
 	token, err := br.accessTokens.Create(ctx, x.ID, x.Type)
 	if err != nil {
-		return resWrapper(nil, err)
+		return jsendWrapper(nil, ERROR, err.Error())
 	}
-	data := map[string]*string{"accessToken": token}
-	return resWrapper(data)
+
+	return jsendWrapper(token, SUCCESS, "")
 }
 
-func (br *BlockchainReactor) listAccessTokens(ctx context.Context) Response {
-	tokens, err := br.accessTokens.List()
+func (br *BlockchainReactor) listAccessTokens(ctx context.Context) interface{} {
+	tokens, err := br.accessTokens.List(ctx)
 	if err != nil {
-		log.Errorf("listAccessTokens: %v", err)
-		return resWrapper(nil, err)
+		return jsendWrapper(nil, ERROR, err.Error())
 	}
 
-	return resWrapper(tokens)
+	return jsendWrapper(tokens, SUCCESS, "")
 }
 
-func (br *BlockchainReactor) deleteAccessToken(ctx context.Context, x struct {
-	ID    string `json:"id"`
-	Token string `json:"token"`
-}) Response {
+func (br *BlockchainReactor) deleteAccessToken(ctx context.Context, x struct{ ID, Token string }) interface{} {
 	//TODO Add delete permission verify.
 	if err := br.accessTokens.Delete(ctx, x.ID); err != nil {
-		return resWrapper(nil, err)
+		return jsendWrapper(nil, ERROR, err.Error())
 	}
-	return resWrapper(nil)
+	return jsendWrapper("success", SUCCESS, "")
 }
 
-func (br *BlockchainReactor) checkAccessToken(ctx context.Context, x struct {
-	ID     string `json:"id"`
-	Secret string `json:"secret"`
-}) Response {
+func (br *BlockchainReactor) checkAccessToken(ctx context.Context, x struct{ ID, Secret string }) interface{} {
 	secret, err := hex.DecodeString(x.Secret)
 	if err != nil {
-		return resWrapper(nil, err)
+		return jsendWrapper(nil, ERROR, err.Error())
 	}
-	_, err = br.accessTokens.Check(ctx, x.ID, secret)
+	result, err := br.accessTokens.Check(ctx, x.ID, secret)
 	if err != nil {
-		return resWrapper(nil, err)
+		return jsendWrapper(nil, ERROR, err.Error())
 	}
-
-	return resWrapper(nil)
+	if result == true {
+		return jsendWrapper("success", SUCCESS, "")
+	}
+	return jsendWrapper("fail", SUCCESS, "")
 }
