cebc8f20452fd07d59c80dc32fab7f03543e7ba7
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2017-12-29 16:11:17 +0800

    Review blockchain 2

--- blockchain/accounts.go
+++ blockchain/accounts.go
@@ -2,85 +2,62 @@ package blockchain
 
 import (
 	"context"
-	"sync"
 
 	"github.com/bytom/blockchain/account"
 	"github.com/bytom/crypto/ed25519/chainkd"
-	"github.com/bytom/net/http/httpjson"
-	"github.com/bytom/net/http/reqid"
 
 	log "github.com/sirupsen/logrus"
 )
 
 // POST /create-account
-func (a *BlockchainReactor) createAccount(ctx context.Context, ins []struct {
-	RootXPubs []chainkd.XPub `json:"root_xpubs"`
-	Quorum    int
-	Alias     string
-	Tags      map[string]interface{}
+func (bcr *BlockchainReactor) createAccount(ctx context.Context, ins struct {
+	RootXPubs []chainkd.XPub         `json:"root_xpubs"`
+	Quorum    int                    `json:"quorum"`
+	Alias     string                 `json:"alias"`
+	Tags      map[string]interface{} `json:"tags"`
 
 	// ClientToken is the application's unique token for the account. Every account
 	// should have a unique client token. The client token is used to ensure
 	// idempotency of create account requests. Duplicate create account requests
 	// with the same client_token will only create one account.
-	ClientToken string `json:"client_token"`
-}) interface{} {
-	responses := make([]interface{}, len(ins))
-	var wg sync.WaitGroup
-	wg.Add(len(responses))
-
-	for i := range responses {
-		go func(i int) {
-			subctx := reqid.NewSubContext(ctx, reqid.New())
-			defer wg.Done()
-			//defer batchRecover(subctx, &responses[i])
-
-			acc, err := a.accounts.Create(subctx, ins[i].RootXPubs, ins[i].Quorum, ins[i].Alias, ins[i].Tags, ins[i].ClientToken)
-			if err != nil {
-				responses[i] = err
-				return
-			}
-			aa, err := account.Annotated(acc)
-			log.WithField("account", aa).Info("Created account")
-			if err != nil {
-				responses[i] = err
-				return
-			}
-			responses[i] = aa
-		}(i)
+	AccessToken string `json:"access_token"`
+}) Response {
+	acc, err := bcr.accounts.Create(nil, ins.RootXPubs, ins.Quorum, ins.Alias, ins.Tags, ins.AccessToken)
+	if err != nil {
+		return resWrapper(nil, err)
 	}
 
-	wg.Wait()
-	log.WithField("responses", responses).Info("Responses of created account")
-	return responses
+	annotatedAccount, err := account.Annotated(acc)
+	if err != nil {
+		return resWrapper(nil, err)
+	}
+
+	log.WithField("account ID", annotatedAccount.ID).Info("Created account")
+
+	return resWrapper(annotatedAccount)
 }
 
 // POST /update-account-tags
-func (a *BlockchainReactor) updateAccountTags(ctx context.Context, ins []struct {
-	ID    *string
-	Alias *string
-	Tags  map[string]interface{} `json:"tags"`
-}) interface{} {
-	log.Info("Updating account tags")
-	responses := make([]interface{}, len(ins))
-	var wg sync.WaitGroup
-	wg.Add(len(responses))
+func (bcr *BlockchainReactor) updateAccountTags(ctx context.Context, updateTag struct {
+	AccountInfo string                 `json:"account_info"`
+	Tags        map[string]interface{} `json:"tags"`
+}) Response {
 
-	for i := range responses {
-		go func(i int) {
-			subctx := reqid.NewSubContext(ctx, reqid.New())
-			defer wg.Done()
-			//defer batchRecover(subctx, &responses[i])
-
-			err := a.accounts.UpdateTags(subctx, ins[i].ID, ins[i].Alias, ins[i].Tags)
-			if err != nil {
-				responses[i] = err
-			} else {
-				responses[i] = httpjson.DefaultResponse
-			}
-		}(i)
+	err := bcr.accounts.UpdateTags(nil, updateTag.AccountInfo, updateTag.Tags)
+	if err != nil {
+		return resWrapper(nil, err)
 	}
 
-	wg.Wait()
-	return responses
+	return resWrapper(nil)
+}
+
+//
+// POST /delete-account
+func (bcr *BlockchainReactor) deleteAccount(ctx context.Context, in struct {
+	AccountInfo string `json:"account_info"`
+}) Response {
+	if err := bcr.accounts.DeleteAccount(in); err != nil {
+		return resWrapper(nil, err)
+	}
+	return resWrapper(nil)
 }
