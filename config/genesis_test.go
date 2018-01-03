ba2bf74357cddf625f6358e47648a0dfc8882135
paladz <453256728@qq.com>
2018-01-03 12:30:56 +0800

    edit the test code struct

--- config/genesis_test.go
+++ config/genesis_test.go
@@ -5,8 +5,14 @@ import (
 )
 
 // test genesis
-func TestGenesis(t *testing.T) {
+func TestGenerateGenesisTx(t *testing.T) {
 	if tx := GenerateGenesisTx(); tx == nil {
 		t.Errorf("Generate genesis tx failed")
+	}
+}
+
+func TestGenerateGenesisBlock(t *testing.T) {
+	if block := GenerateGenesisBlock(); block == nil {
+		t.Errorf("Generate genesis block failed")
 	}
 }
--- integration_test/standard_transaction_test.go
+++ integration_test/standard_transaction_test.go
@@ -58,45 +58,17 @@ func TestP2PKH(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	utxo := &account.UTXO{}
-	utxo.OutputID = bc.Hash{V0: 1}
-	utxo.SourceID = bc.Hash{V0: 2}
-	utxo.AssetID = *consensus.BTMAssetID
-	utxo.Amount = 1000000000
-	utxo.SourcePos = 0
-	utxo.ControlProgram = controlProg.ControlProgram
-	utxo.AccountID = controlProg.AccountID
-	utxo.Address = controlProg.Address
-	utxo.ControlProgramIndex = controlProg.KeyIndex
-	txInput, sigInst, err := account.UtxoToInputs(testAccount.Signer, utxo, nil)
+	utxo := mockUTXO(controlProg)
+	tpl, tx, err := mockTx(utxo, testAccount)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	b := txbuilder.NewBuilder(time.Now())
-	b.AddInput(txInput, sigInst)
-	out := legacy.NewTxOutput(*consensus.BTMAssetID, 100, []byte{byte(vm.OP_FAIL)}, nil)
-	b.AddOutput(out)
-	tpl, tx, err := b.Build()
-	if err != nil {
+	if err := mockSign(tpl, hsm); err != nil {
 		t.Fatal(err)
 	}
 
-	err = txbuilder.Sign(nil, tpl, nil, "password", func(_ context.Context, xpub chainkd.XPub, path [][]byte, data [32]byte, password string) ([]byte, error) {
-		sigBytes, err := hsm.XSign(xpub, path, data[:], password)
-		if err != nil {
-			return nil, nil
-		}
-		return sigBytes, err
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bcBlock := &bc.Block{
-		BlockHeader: &bc.BlockHeader{Height: 1},
-	}
-	if _, err = validation.ValidateTx(legacy.MapTx(tx), bcBlock); err != nil {
+	if _, err = validation.ValidateTx(legacy.MapTx(tx), mockBlock()); err != nil {
 		t.Fatal(err)
 	}
 }
@@ -142,45 +114,17 @@ func TestP2SH(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	utxo := &account.UTXO{}
-	utxo.OutputID = bc.Hash{V0: 1}
-	utxo.SourceID = bc.Hash{V0: 2}
-	utxo.AssetID = *consensus.BTMAssetID
-	utxo.Amount = 1000000000
-	utxo.SourcePos = 0
-	utxo.ControlProgram = controlProg.ControlProgram
-	utxo.AccountID = controlProg.AccountID
-	utxo.Address = controlProg.Address
-	utxo.ControlProgramIndex = controlProg.KeyIndex
-	txInput, sigInst, err := account.UtxoToInputs(testAccount.Signer, utxo, nil)
+	utxo := mockUTXO(controlProg)
+	tpl, tx, err := mockTx(utxo, testAccount)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	b := txbuilder.NewBuilder(time.Now())
-	b.AddInput(txInput, sigInst)
-	out := legacy.NewTxOutput(*consensus.BTMAssetID, 100, []byte{byte(vm.OP_FAIL)}, nil)
-	b.AddOutput(out)
-	tpl, tx, err := b.Build()
-	if err != nil {
+	if err := mockSign(tpl, hsm); err != nil {
 		t.Fatal(err)
 	}
 
-	err = txbuilder.Sign(nil, tpl, nil, "password", func(_ context.Context, xpub chainkd.XPub, path [][]byte, data [32]byte, password string) ([]byte, error) {
-		sigBytes, err := hsm.XSign(xpub, path, data[:], password)
-		if err != nil {
-			return nil, nil
-		}
-		return sigBytes, err
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bcBlock := &bc.Block{
-		BlockHeader: &bc.BlockHeader{Height: 1},
-	}
-	if _, err = validation.ValidateTx(legacy.MapTx(tx), bcBlock); err != nil {
+	if _, err = validation.ValidateTx(legacy.MapTx(tx), mockBlock()); err != nil {
 		t.Fatal(err)
 	}
 }
@@ -193,4 +137,47 @@ func mockChain(testDB dbm.DB) (*protocol.Chain, error) {
 		return nil, err
 	}
 	return chain, nil
+}
+
+func mockUTXO(controlProg *account.CtrlProgram) *account.UTXO {
+	utxo := &account.UTXO{}
+	utxo.OutputID = bc.Hash{V0: 1}
+	utxo.SourceID = bc.Hash{V0: 2}
+	utxo.AssetID = *consensus.BTMAssetID
+	utxo.Amount = 1000000000
+	utxo.SourcePos = 0
+	utxo.ControlProgram = controlProg.ControlProgram
+	utxo.AccountID = controlProg.AccountID
+	utxo.Address = controlProg.Address
+	utxo.ControlProgramIndex = controlProg.KeyIndex
+	return utxo
+}
+
+func mockTx(utxo *account.UTXO, testAccount *account.Account) (*txbuilder.Template, *legacy.TxData, error) {
+	txInput, sigInst, err := account.UtxoToInputs(testAccount.Signer, utxo, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	b := txbuilder.NewBuilder(time.Now())
+	b.AddInput(txInput, sigInst)
+	out := legacy.NewTxOutput(*consensus.BTMAssetID, 100, []byte{byte(vm.OP_FAIL)}, nil)
+	b.AddOutput(out)
+	return b.Build()
+}
+
+func mockSign(tpl *txbuilder.Template, hsm *pseudohsm.HSM) error {
+	return txbuilder.Sign(nil, tpl, nil, "password", func(_ context.Context, xpub chainkd.XPub, path [][]byte, data [32]byte, password string) ([]byte, error) {
+		sigBytes, err := hsm.XSign(xpub, path, data[:], password)
+		if err != nil {
+			return nil, nil
+		}
+		return sigBytes, err
+	})
+}
+
+func mockBlock() *bc.Block {
+	return &bc.Block{
+		BlockHeader: &bc.BlockHeader{Height: 1},
+	}
 }
