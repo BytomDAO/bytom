a048580fc199ba7de2ec2f92d49706665e1354ca
Yongfeng LI <wliyongfeng@gmail.com>
2018-01-10 19:06:33 +0800

    refactor create-account-receiver

--- blockchain/receivers.go
+++ blockchain/receivers.go
@@ -2,20 +2,18 @@ package blockchain
 
 import (
 	"context"
-	"time"
 )
 
 // POST /create-account-receiver
 func (bcr *BlockchainReactor) createAccountReceiver(ctx context.Context, ins struct {
 	AccountInfo string    `json:"account_info"`
-	ExpiresAt   time.Time `json:"expires_at,omitempty"`
 }) Response {
-	receiver, err := bcr.accounts.CreateReceiver(nil, ins.AccountInfo, ins.ExpiresAt)
+	receiver, err := bcr.accounts.CreateReceiver(nil, ins.AccountInfo)
 	if err != nil {
-		return resWrapper(nil, err)
+		return NewErrorResponse(err)
 	}
 
-	return resWrapper(*receiver)
+	return NewSuccessResponse(*receiver)
 }
 
 func (bcr *BlockchainReactor) createAccountAddress(ctx context.Context, ins struct {
