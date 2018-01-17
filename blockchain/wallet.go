0b30ca887ffd6abe3941caa89a9f1afd57cc9143
李永峰 <wliyongfeng@gmail.com>
2018-01-17 18:12:41 +0800

    add export private key (#299)

--- blockchain/wallet.go
+++ blockchain/wallet.go
@@ -10,15 +10,15 @@ func (bcr *BlockchainReactor) walletExportKey(ctx context.Context, in struct {
 	Password string       `json:"password"`
 	XPub     chainkd.XPub `json:"xpub"`
 }) Response {
+	key, err := bcr.wallet.ExportAccountPrivKey(bcr.hsm, in.XPub, in.Password)
+	if err != nil {
+		return NewErrorResponse(err)
+	}
+
 	type privateKey struct {
 		PrivateKey string `json:"private_key"`
 	}
-
-	key, err := bcr.wallet.ExportAccountPrivKey(bcr.hsm, in.XPub, in.Password)
-	if err != nil {
-		return resWrapper(nil, err)
-	}
-	return resWrapper(&privateKey{PrivateKey: *key})
+	return NewSuccessResponse(privateKey{PrivateKey: *key})
 }
 
 func (bcr *BlockchainReactor) walletImportKey(ctx context.Context, in struct {
@@ -30,7 +30,7 @@ func (bcr *BlockchainReactor) walletImportKey(ctx context.Context, in struct {
 }) Response {
 	xpub, err := bcr.wallet.ImportAccountPrivKey(bcr.hsm, in.XPrv, in.KeyAlias, in.Password, in.Index, in.AccountAlias)
 	if err != nil {
-		return resWrapper(nil, err)
+		return NewErrorResponse(err)
 	}
-	return resWrapper(xpub)
+	return NewSuccessResponse(xpub)
 }
