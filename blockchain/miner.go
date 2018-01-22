f086465454a58af624f345f66207e848fd4c6694
gguoss <1536310027@qq.com>
2018-01-22 17:42:42 +0800

    Add marshal function

--- blockchain/miner.go
+++ blockchain/miner.go
@@ -1,6 +1,9 @@
 package blockchain
 
 import (
+	"fmt"
+	"encoding/json"
+
 	"github.com/bytom/mining"
 	"github.com/bytom/protocol/bc/legacy"
 )
@@ -17,12 +20,50 @@ func (bcr *BlockchainReactor) getWork() Response {
 	if seedCache, err := seedCaches.Get(&resp.Header.Seed); err != nil {
 		return NewErrorResponse(err)
 	} else {
-		resp.cache = seedCache
+		fmt.Printf("----seed cashe:%v\n", seedCache)
+		resp.Cache = seedCache
 	}
-	return NewSuccessResponse(resp)
+	if res, err := resp.MarshalJSON(); err == nil {
+		fmt.Printf("---------res:%v\n", res)
+		var test WorkResp
+		err = test.UnmarshalJSON(res)
+		fmt.Printf("----------test:%v, err:%v\n", test, err)
+		return NewSuccessResponse(res)
+	} else {
+		return NewErrorResponse(err)
+	}
 }
 
 type WorkResp struct {
-	Header legacy.BlockHeader `json:"header"`
-	cache  []uint32       `json:"cache"`
+	Header legacy.BlockHeader
+	Cache  []uint32
+}
+
+type WorkByte struct {
+	ByteHeader []byte `json:"header"`
+	Cache	   []uint32 `json:"cache"`
+}
+
+func (work *WorkResp) UnmarshalJSON(b []byte) error {
+	var workByte WorkByte
+	if err := json.Unmarshal(b, &workByte); err != nil {
+		return err
+	}
+
+	if err := work.Header.UnmarshalText(workByte.ByteHeader); err != nil {
+		return err
+	}
+	work.Cache = workByte.Cache
+
+	return nil
+}
+
+func (work *WorkResp) MarshalJSON() ([]byte, error) {
+	var workByte WorkByte
+	var err error
+	workByte.ByteHeader, err = work.Header.MarshalText()
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(workByte)
 }
--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bytom/util"
 	"github.com/bytom/blockchain"
+//	"github.com/bytom/protocol/bc/legacy"
 )
 
 func doWork(work *blockchain.WorkResp) {
@@ -13,10 +14,14 @@ func doWork(work *blockchain.WorkResp) {
 
 func main() {
 	data, exitCode := util.ClientCall("/get-work")
+	fmt.Printf("data:%v", data)
 	if exitCode != util.Success {
 		return
 	}
-	if work, ok := data.(blockchain.WorkResp); ok {
+/*	var work blockchain.WorkResp
+	if dataMap, ok := data.(map[string]interface{}); ok {
+		work.Header = dataMap["header"].(legacy.BlockHeader)
+		fmt.Printf("work:%v", work)
 		doWork(&work)
-	}
+	}*/
 }
