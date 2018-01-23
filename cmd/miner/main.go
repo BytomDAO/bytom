323da7e1f95341d44495dba3d1ba391ea831c3fb
gguoss <1536310027@qq.com>
2018-01-23 09:45:50 +0800

    miner getwork and do work ok

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -4,11 +4,33 @@ import (
 	"fmt"
 
 	"github.com/bytom/blockchain"
+	"github.com/bytom/consensus/algorithm"
+	"github.com/bytom/consensus/difficulty"
 	"github.com/bytom/util"
 )
 
+const (
+	maxNonce = ^uint64(0) // 2^32 - 1
+)
+
+// do proof of work
 func doWork(work *blockchain.WorkResp) {
 	fmt.Printf("work:%v\n", work)
+	for i := uint64(0); i <= maxNonce; i++ {
+		work.Header.Nonce = i
+		headerHash := work.Header.Hash()
+		proofHash, err := algorithm.AIHash(work.Header.Height, &headerHash, work.Cache)
+		if err != nil {
+			fmt.Printf("Mining: failed on AIHash: %v\n", err)
+			return
+		}
+
+		if difficulty.CheckProofOfWork(proofHash, work.Header.Bits) {
+			// to do: submitWork
+			fmt.Printf("Mining: successful-----proof hash:%v\n", proofHash)
+			return
+		}
+	}
 }
 
 func main() {
