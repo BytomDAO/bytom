884d07cdef100d8c8ce959b18909b8266d1591d4
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2018-01-25 13:52:40 +0800

    InitNativeAsset before NewRegistry

--- node/node.go
+++ node/node.go
@@ -190,8 +190,8 @@ func NewNode(config *cfg.Config) *Node {
 	if !config.Wallet.Disable {
 		walletDB := dbm.NewDB("wallet", config.DBBackend, config.DBDir())
 		accounts = account.NewManager(walletDB, chain)
-		assets = asset.NewRegistry(walletDB, chain)
 		asset.InitNativeAsset()
+		assets = asset.NewRegistry(walletDB, chain)
 		wallet, err = w.NewWallet(walletDB, accounts, assets, chain)
 		if err != nil {
 			log.WithField("error", err).Error("init NewWallet")
