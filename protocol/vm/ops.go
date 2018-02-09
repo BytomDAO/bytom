dffa3923cc342e7b16aa259a5944ce43bd405e32
oysheng <oysheng@bytom.io>
2018-02-09 14:08:04 +0800

    add this func because of ivy compiler generate contract instance

--- protocol/vm/ops.go
+++ protocol/vm/ops.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math"
+	"reflect"
 
 	"github.com/bytom/errors"
 	"github.com/bytom/math/checked"
@@ -484,4 +485,13 @@ func init() {
 			isExpansion[i] = true
 		}
 	}
+}
+
+// IsPushdata judge instruction whether is a pushdata operation(include opFalse operation)
+func (inst *Instruction) IsPushdata() bool {
+	if reflect.ValueOf(ops[inst.Op].fn) == reflect.ValueOf(ops[OP_1].fn) || inst.Op == OP_FALSE {
+		return true
+	}
+
+	return false
 }
