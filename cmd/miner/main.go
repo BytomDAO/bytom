fece5070f9b5600e07c8723ad595120bea7bf248
paladz <453256728@qq.com>
2018-03-08 11:14:18 +0800

    change the api url

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -28,7 +28,7 @@ func doWork(bh *legacy.BlockHeader) bool {
 }
 
 func main() {
-	data, _ := util.ClientCall("/mining-pool/get-work", nil)
+	data, _ := util.ClientCall("/mining-pool/getwork", nil)
 	rawData, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println(err)
@@ -41,7 +41,7 @@ func main() {
 	}
 
 	if doWork(bh) {
-		util.ClientCall("/mining-pool/submit-work", &bh)
+		util.ClientCall("/mining-pool/submitwork", &bh)
 	}
 
 }
