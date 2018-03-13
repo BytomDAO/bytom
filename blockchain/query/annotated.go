3bd1fc521ed7084d8cf3aaf0f9492d4661a12210
Yongfeng LI <wliyongfeng@gmail.com>
2018-03-13 15:19:10 +0800

    fix typo

--- blockchain/query/annotated.go
+++ blockchain/query/annotated.go
@@ -27,7 +27,7 @@ type AnnotatedTx struct {
 type AnnotatedInput struct {
 	Type            string              `json:"type"`
 	AssetID         bc.AssetID          `json:"asset_id"`
-	AssetAlias      string              `json:"asset_alias,omitempty"TestBuild`
+	AssetAlias      string              `json:"asset_alias,omitempty"`
 	AssetDefinition *chainjson.HexBytes `json:"asset_definition"`
 	Amount          uint64              `json:"amount"`
 	IssuanceProgram chainjson.HexBytes  `json:"issuance_program,omitempty"`
