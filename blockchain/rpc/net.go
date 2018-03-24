b1da0f6a0380483b45d7ae8a9d992d0b081f3ff3
Yongfeng LI <wliyongfeng@gmail.com>
2018-03-24 11:05:12 +0800

    Revert "remove rpc package (#474)" (#475)
    This reverts commit c0563a5d0953c00d86885c4d11d0ca7fdb19c090.

---
+++ blockchain/rpc/net.go
@@ -0,0 +1,28 @@
+package rpc
+
+import (
+	ctypes "github.com/bytom/blockchain/rpc/types"
+	"github.com/bytom/p2p"
+)
+
+// NetInfo return p2p net status
+func NetInfo(p2pSwitch *p2p.Switch) (*ctypes.ResultNetInfo, error) {
+	listening := p2pSwitch.IsListening()
+	listeners := []string{}
+	for _, listener := range p2pSwitch.Listeners() {
+		listeners = append(listeners, listener.String())
+	}
+	peers := []ctypes.Peer{}
+	for _, peer := range p2pSwitch.Peers().List() {
+		peers = append(peers, ctypes.Peer{
+			NodeInfo:         *peer.NodeInfo,
+			IsOutbound:       peer.IsOutbound(),
+			ConnectionStatus: peer.Connection().Status(),
+		})
+	}
+	return &ctypes.ResultNetInfo{
+		Listening: listening,
+		Listeners: listeners,
+		Peers:     peers,
+	}, nil
+}
