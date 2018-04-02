82be6a35713f9f45a7b7f5a18f43a6d5b9578e08
oysheng <oysheng@bytom.io>
2018-04-02 16:00:14 +0800

    optimse api test

--- api/api_test.go
+++ api/api_test.go
@@ -14,15 +14,8 @@ import (
 )
 
 func TestAPIHandler(t *testing.T) {
-	// the TestResponse is same with api Response
-	type TestResponse struct {
-		Status string      `json:"status,omitempty"`
-		Msg    string      `json:"msg,omitempty"`
-		Data   interface{} `json:"data,omitempty"`
-	}
-
 	a := &API{}
-	response := &TestResponse{}
+	response := &Response{}
 
 	// init httptest server
 	a.buildHandler()
@@ -42,7 +35,7 @@ func TestAPIHandler(t *testing.T) {
 	cases := []struct {
 		path        string
 		request     interface{}
-		respWant    *TestResponse
+		respWant    *Response
 		respWantErr error
 	}{
 		{
@@ -51,7 +44,7 @@ func TestAPIHandler(t *testing.T) {
 				Alias    string `json:"alias"`
 				Password string `json:"password"`
 			}{Alias: "alice", Password: "123456"},
-			respWant: &TestResponse{
+			respWant: &Response{
 				Status: "fail",
 				Msg:    "wallet not found, please check that the wallet is open",
 			},
@@ -59,15 +52,18 @@ func TestAPIHandler(t *testing.T) {
 		{
 			path:    "/error",
 			request: nil,
-			respWant: &TestResponse{
+			respWant: &Response{
 				Status: "fail",
 				Msg:    "wallet not found, please check that the wallet is open",
 			},
 		},
 		{
-			path:     "/",
-			request:  nil,
-			respWant: &TestResponse{Status: "", Msg: "", Data: interface{}(nil)},
+			path:    "/",
+			request: nil,
+			respWant: &Response{
+				Status: "",
+				Msg:    "",
+			},
 		},
 		{
 			path: "/create-access-token",
@@ -75,7 +71,7 @@ func TestAPIHandler(t *testing.T) {
 				ID   string `json:"id"`
 				Type string `json:"type"`
 			}{ID: "test-access-id", Type: "test-access-type"},
-			respWant: &TestResponse{
+			respWant: &Response{
 				Status: "success",
 				Msg:    "",
 				Data:   map[string]interface{}{"id": "test-access-id", "type": "test-access-type", "token": "test-access-id:440d87ae0d625a7fcf076275b18372e09a0899e37ec86398879388de90cb0c67"},
@@ -84,7 +80,7 @@ func TestAPIHandler(t *testing.T) {
 		{
 			path:    "/gas-rate",
 			request: nil,
-			respWant: &TestResponse{
+			respWant: &Response{
 				Status: "success",
 				Msg:    "",
 				Data:   map[string]interface{}{"gasRate": 1000},
@@ -93,7 +89,7 @@ func TestAPIHandler(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		response = &TestResponse{}
+		response = &Response{}
 		client.Call(context.Background(), c.path, c.request, &response)
 
 		if !testutil.DeepEqual(response.Status, c.respWant.Status) {
