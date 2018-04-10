c95cf5fa5207026e6d95d72c4596e170da533ec9
Yongfeng LI <wliyongfeng@gmail.com>
2018-04-10 10:58:12 +0800

    update version (#565)

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// The full version string
-	Version = "0.4.2"
+	Version = "0.4.3"
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
 )
