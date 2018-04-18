e7609a05eccbe0e7ba5384220c0024d2286e4e8d
HAOYUatHZ <haoyu@protonmail.com>
2018-04-19 07:21:34 +0800

    update for dylib

--- mining/tensority/dylib/algorithm.go
+++ mining/tensority/dylib/algorithm.go
@@ -9,9 +9,9 @@ package tensority
 #include <dlfcn.h>
 
 typedef uint8_t* (*CSIMDTS_FUNC)(uint8_t*, uint8_t*, uint8_t*);
-#define LINUX_LIB_CSIMDTS_PATH "./cSimdTs.dylib"
+#define DYLIB_CSIMDTS_PATH "./cSimdTs.dylib"
 
-int linux_csimdts(uint8_t blockheader[32],
+int dylib_csimdts(uint8_t blockheader[32],
                     uint8_t seed[32],
                     uint8_t res[32]){
     void *handle;
@@ -19,7 +19,7 @@ int linux_csimdts(uint8_t blockheader[32],
     CSIMDTS_FUNC csimdts_func = NULL;
 
     //open the dynamic lib
-    handle = dlopen(LINUX_LIB_CSIMDTS_PATH, RTLD_NOW);
+    handle = dlopen(DYLIB_CSIMDTS_PATH, RTLD_NOW);
     if (!handle) {
         fprintf(stderr, "%s\n", dlerror());
         return 1;
@@ -49,31 +49,18 @@ import(
     "github.com/bytom/protocol/bc"
 )
 
-// func Hash(blockHeader [32]uint8, seed [32]uint8) [32]uint8 {
-//     var res [32]uint8
-
-//     bhPtr := (*C.uchar)(unsafe.Pointer(&blockHeader))
-//     seedPtr := (*C.uchar)(unsafe.Pointer(&seed))
-//     resPtr := (*C.uchar)(unsafe.Pointer(&res))
-
-//     C.linux_csimdts(bhPtr, seedPtr, resPtr)
-
-//     res = *(*[32]uint8)(unsafe.Pointer(resPtr))
-//     return res
-// }
-
 
 func Hash(blockHeader, seed *bc.Hash) *bc.Hash {
     var resAddr [32]uint8
     bhBytes := blockHeader.Bytes()
     sdBytes := seed.Bytes()
 
-    // Get thearray pointer from the corresponding slice
+    // Get the array pointer from the corresponding slice
     bhPtr := (*C.uchar)(unsafe.Pointer(&bhBytes[0]))
     seedPtr := (*C.uchar)(unsafe.Pointer(&sdBytes[0]))
     resPtr := (*C.uchar)(unsafe.Pointer(&resAddr))
 
-    C.linux_csimdts(bhPtr, seedPtr, resPtr)
+    C.dylib_csimdts(bhPtr, seedPtr, resPtr)
     
     resHash := bc.NewHash(*(*[32]byte)(unsafe.Pointer(resPtr)))
     return &resHash
