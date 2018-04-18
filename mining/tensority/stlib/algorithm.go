49a3cf75696322544f4134122d2a9350d14dd4e4
HAOYUatHZ <haoyu@protonmail.com>
2018-04-19 07:01:05 +0800

    merge static lib

--- mining/tensority/stlib/algorithm.go
+++ mining/tensority/stlib/algorithm.go
@@ -8,10 +8,14 @@ import "C"
 import(
     "unsafe"
 
+    "github.com/golang/groupcache/lru"
+    "github.com/bytom/crypto/sha3pool"
     "github.com/bytom/protocol/bc"
 )
 
-func Hash(blockHeader, seed *bc.Hash) *bc.Hash {
+const maxAIHashCached = 64
+
+func algorithm(blockHeader, seed *bc.Hash) *bc.Hash {
     bhBytes := blockHeader.Bytes()
     sdBytes := seed.Bytes()
 
@@ -24,3 +28,38 @@ func Hash(blockHeader, seed *bc.Hash) *bc.Hash {
     res := bc.NewHash(*(*[32]byte)(unsafe.Pointer(resPtr)))
     return &res
 }
+
+func calcCacheKey(hash, seed *bc.Hash) *bc.Hash {
+    var b32 [32]byte
+    sha3pool.Sum256(b32[:], append(hash.Bytes(), seed.Bytes()...))
+    key := bc.NewHash(b32)
+    return &key
+}
+
+// Cache is create for cache the tensority result
+type Cache struct {
+    lruCache *lru.Cache
+}
+
+// NewCache create a cache struct
+func NewCache() *Cache {
+    return &Cache{lruCache: lru.New(maxAIHashCached)}
+}
+
+// AddCache is used for add tensority calculate result
+func (a *Cache) AddCache(hash, seed, result *bc.Hash) {
+    key := calcCacheKey(hash, seed)
+    a.lruCache.Add(*key, result)
+}
+
+// Hash is the real entry for call tensority algorithm
+func (a *Cache) Hash(hash, seed *bc.Hash) *bc.Hash {
+    key := calcCacheKey(hash, seed)
+    if v, ok := a.lruCache.Get(*key); ok {
+        return v.(*bc.Hash)
+    }
+    return algorithm(hash, seed)
+}
+
+// AIHash is created for let different package share same cache
+var AIHash = NewCache()
