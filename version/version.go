5fda46775d18d62cb32293a718dd7e0fcee478bb
paladz <453256728@qq.com>
2018-04-28 13:43:12 +0800

    edit version for prod

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// The full version string
-	Version = "1.0.2"
+	Version = "1.0.1"
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
 )
