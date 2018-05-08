7254c87d4c90c03e9b6e499ef53337665b4d3696
Yahtoo Ma <yahtoo.ma@gmail.com>
2018-05-08 16:04:33 +0800

    Add peer close connection waite

--- p2p/pex_reactor.go
+++ p2p/pex_reactor.go
@@ -128,10 +128,14 @@ func (r *PEXReactor) AddPeer(p *Peer) error {
 	// close the connect if connect is big than max limit
 	if r.sw.peers.Size() >= r.sw.config.MaxNumPeers {
 		if ok := r.SendAddrs(p, r.book.GetSelection()); ok {
-			r.sw.StopPeerGracefully(p)
+			select {
+			case <-time.After(1 * time.Second):
+				r.sw.StopPeerGracefully(p)
+			}
 		}
 		return errors.New("Error in AddPeer: reach the max peer, exchange then close")
 	}
+
 	return nil
 }
 
