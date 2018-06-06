96adc117551baa71a5f65af02f2bd85d3a2b42c6
paladz <453256728@qq.com>
2018-06-07 00:46:08 +0800

    check the switch level logic

--- p2p/node_info.go
+++ p2p/node_info.go
@@ -26,34 +26,25 @@ type NodeInfo struct {
 // CONTRACT: two nodes are compatible if the major version matches and network match
 // and they have at least one channel in common.
 func (info *NodeInfo) CompatibleWith(other *NodeInfo) error {
-	iMajor, iMinor, _, iErr := splitVersion(info.Version)
-	oMajor, oMinor, _, oErr := splitVersion(other.Version)
-
-	// if our own version number is not formatted right, we messed up
-	if iErr != nil {
-		return iErr
+	iMajor, iMinor, _, err := splitVersion(info.Version)
+	if err != nil {
+		return err
 	}
 
-	// version number must be formatted correctly ("x.x.x")
-	if oErr != nil {
-		return oErr
+	oMajor, oMinor, _, err := splitVersion(other.Version)
+	if err != nil {
+		return err
 	}
 
-	// major version must match
 	if iMajor != oMajor {
 		return fmt.Errorf("Peer is on a different major version. Got %v, expected %v", oMajor, iMajor)
 	}
-
-	// minor version must match
 	if iMinor != oMinor {
 		return fmt.Errorf("Peer is on a different minor version. Got %v, expected %v", oMinor, iMinor)
 	}
-
-	// nodes must be on the same network
 	if info.Network != other.Network {
 		return fmt.Errorf("Peer is on a different network. Got %v, expected %v", other.Network, info.Network)
 	}
-
 	return nil
 }
 
--- p2p/public_ip.go
+++ p2p/public_ip.go
@@ -20,15 +20,17 @@ var ipCheckServices = []string{
 	"http://myexternalip.com/raw",
 }
 
-type IpResult struct {
+// IPResult is the ip check response
+type IPResult struct {
 	Success bool
-	Ip      string
+	IP      string
 }
 
 var timeout = time.Duration(5)
 
-func GetIP() *IpResult {
-	resultCh := make(chan *IpResult, 1)
+// GetIP return the ip of the current host
+func GetIP() *IPResult {
+	resultCh := make(chan *IPResult, 1)
 	for _, s := range ipCheckServices {
 		go ipAddress(s, resultCh)
 	}
@@ -38,12 +40,12 @@ func GetIP() *IpResult {
 		case result := <-resultCh:
 			return result
 		case <-time.After(time.Second * timeout):
-			return &IpResult{false, ""}
+			return &IPResult{false, ""}
 		}
 	}
 }
 
-func ipAddress(service string, done chan<- *IpResult) {
+func ipAddress(service string, done chan<- *IPResult) {
 	client := http.Client{Timeout: time.Duration(timeout * time.Second)}
 	resp, err := client.Get(service)
 	if err != nil {
@@ -59,7 +61,7 @@ func ipAddress(service string, done chan<- *IpResult) {
 	address := strings.TrimSpace(string(data))
 	if ip := net.ParseIP(address); ip != nil && ip.To4() != nil {
 		select {
-		case done <- &IpResult{true, address}:
+		case done <- &IPResult{true, address}:
 			return
 		default:
 			return
