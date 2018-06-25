0cdd73a83566cf28fe0c6a28e359aef3e66c4dc0
oysheng <33340252+oysheng@users.noreply.github.com>
2018-06-25 20:05:05 +0800

    refactor spend_account_unspent_output struct for add contract arguments (#1089)
    * refactor spend_account_unspent_output struct for add contract arguments
    add the contract template LockWithPublicKey for build-transaction
    
    * optimise arguments
    add unit test
    
    * optimise unit test
    
    * optimise struct

---
+++ cmd/bytomcli/commands/template.go
@@ -0,0 +1,16 @@
+package commands
+
+// contract is LockWithPublicKey
+var buildLockWithPublicKeyReqFmt = `
+	{"actions": [
+		{"type": "spend_account_unspent_output", "output_id": "%s", "arguments": [{"type": "raw_tx_signature", "raw_data": {derivation_path": ["%s", "%s"], "xpub": "%s"}}]},
+		{"type": "control_program", "asset_id": "%s", "amount": %s, "control_program": "%s"},
+		{"type": "spend_account", "asset_id": "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "amount": %s, "account_id": "%s"}
+	]}`
+
+var buildLockWithPublicKeyReqFmtByAlias = `
+	{"actions": [
+		{"type": "spend_account_unspent_output", "output_id": "%s", "arguments": [{"type": "raw_tx_signature", "raw_data": {"derivation_path": ["%s", "%s"], "xpub": "%s"}}]},
+		{"type": "control_program", "asset_alias": "%s", "amount": %s, "control_program": "%s"},
+		{"type": "spend_account", "asset_alias": "BTM", "amount": %s, "account_alias": "%s"}
+	]}`
