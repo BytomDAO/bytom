6ddd2d4550458f1e15eaf3d4bd48ee837a5c95be
Chengcheng Zhang <zcc0721@foxmail.com>
2018-06-26 19:45:53 +0800

    add sm2_verify.go

--- crypto/sm2/sm2.go
+++ crypto/sm2/sm2.go
@@ -30,7 +30,7 @@ import (
 	"io"
 	"math/big"
 
-	"github.com/tjfoc/gmsm/sm3"
+	"github.com/bytom/crypto/sm3"
 )
 
 const (
---
+++ crypto/sm2/sm2_verify.go
@@ -0,0 +1,6 @@
+package sm2
+
+// Sum2VerifyBytes() verify sigature is valid. The parameters is bytes.
+func Sm2VerifyBytes() bool {
+
+}
