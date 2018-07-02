a429e504ab2f563f0cdc10f7d7ee5079bab73072
oysheng <33340252+oysheng@users.noreply.github.com>
2018-07-02 17:04:04 +0800

    delete time waiter (#1111)

--- api/wallet.go
+++ api/wallet.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"context"
-	"time"
 
 	"github.com/bytom/account"
 	"github.com/bytom/asset"
@@ -60,13 +59,6 @@ func (a *API) backupWalletImage() Response {
 
 func (a *API) rescanWallet() Response {
 	a.wallet.RescanBlocks()
-	for {
-		time.Sleep(50 * time.Microsecond)
-		walletStatus := a.wallet.GetWalletStatusInfo()
-		if walletStatus.WorkHeight != walletStatus.BestHeight {
-			break
-		}
-	}
 	return NewSuccessResponse(nil)
 }
 
