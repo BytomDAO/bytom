d4a5ff99d1a2002330e1d6cf1a154b91724ac5a1
HAOYUatHZ <haoyu@protonmail.com>
2018-07-18 17:39:32 +0800

    Go fmt

--- mining/tensority/algorithm_other.go
+++ mining/tensority/algorithm_other.go
@@ -3,12 +3,12 @@
 package tensority
 
 import (
-    log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 
-    "github.com/bytom/protocol/bc"
+	"github.com/bytom/protocol/bc"
 )
 
 func simdAlgorithm(bh, seed *bc.Hash) *bc.Hash {
-    log.Warn("The SIMD-version is not supported on releases, please compile by your yourself to enable this feature.")
-    return legacyAlgorithm(bh, seed)
+	log.Warn("The SIMD-version is not supported on releases, please compile by your yourself to enable this feature.")
+	return legacyAlgorithm(bh, seed)
 }
