06220aa59a76fd4eab82fbab4375f0902f8b3aa7
Chengcheng Zhang <943420582@qq.com>
2018-07-27 14:36:18 +0800

    fix import

--- crypto/sm2/x509.go
+++ crypto/sm2/x509.go
@@ -42,9 +42,10 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/tjfoc/gmsm/sm3"
 	"golang.org/x/crypto/ripemd160"
 	"golang.org/x/crypto/sha3"
+
+	"github.com/bytom/crypto/sm3"
 )
 
 // pkixPublicKey reflects a PKIX public key structure. See SubjectPublicKeyInfo
