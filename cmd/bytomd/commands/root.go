d75eeddbd9680e5aee6a1e894fca481085a56215
HAOYUatHZ <haoyu@protonmail.com>
2018-08-13 16:17:14 +0800

    Fix $HOME env

--- cmd/bytomd/commands/root.go
+++ cmd/bytomd/commands/root.go
@@ -1,8 +1,12 @@
 package commands
 
 import (
+	"os/user"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	cmn "github.com/tendermint/tmlibs/common"
 
 	cfg "github.com/bytom/config"
 )
@@ -18,6 +22,15 @@ var RootCmd = &cobra.Command{
 		err := viper.Unmarshal(config)
 		if err != nil {
 			return err
+		}
+		paths := strings.SplitN(config.RootDir, "/", 2)
+		if len(paths) == 2 && (paths[0] == "~" || paths[0] == "$HOME") {
+			usr, err := user.Current()
+			if err != nil {
+				cmn.Exit("Error: " + err.Error())
+			}
+			paths[0] = usr.HomeDir
+			config.RootDir = paths[0] + "/" + paths[1]
 		}
 		config.SetRoot(config.RootDir)
 		return nil
