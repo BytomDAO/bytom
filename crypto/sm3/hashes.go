07e07699b938056fb0e3f15421639c5faac413fb
Chengcheng Zhang <943420582@qq.com>
2018-08-14 17:06:32 +0800

    update

--- crypto/sm3/hashes.go
+++ crypto/sm3/hashes.go
@@ -7,11 +7,3 @@ func Sum256(data []byte) (digest []byte) {
 	h.Sum(digest[:0])
 	return
 }
-
-// // Sum256 returns the SHA3-256 digest of the data.
-// func Sum256(data []byte) (digest [32]byte) {
-// 	h := New256()
-// 	h.Write(data)
-// 	h.Sum(digest[:0])
-// 	return
-// }
