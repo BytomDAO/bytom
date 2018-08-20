d397688e3d61dbbb64caa04f9bd07d60d604fbf1
HAOYUatHZ <haoyu@protonmail.com>
2018-08-20 16:08:48 +0800

    :zap: Pause

--- version/version.go
+++ version/version.go
@@ -1,5 +1,10 @@
 package version
 
+import (
+	// "fmt"
+	gover "github.com/hashicorp/go-version"
+)
+
 const (
 	deprecateBelow = "1.0.0"
 	notifyLimit    = uint16(3)
@@ -18,6 +23,7 @@ func init() {
 	if GitCommit != "" {
 		Version += "-" + GitCommit[:8]
 	}
+	// maxVerSeen, _ = gover.NewVersion(Version)
 	maxVerSeen, _ = parse(Version)
 }
 
@@ -32,15 +38,16 @@ func init() {
 // | 1.0.3 | same major&moinor version. |
 // | 1.0.4 |     same major version.    |
 func CompatibleWith(remoteVer string) (bool, error) {
-	localVerNum, err := parse(Version)
-	if err != nil {
-		return false, err
-	}
-	remoteVerNum, err := parse(remoteVer)
-	if err != nil {
-		return false, err
-	}
-	return (localVerNum.major == remoteVerNum.major), nil
+	// localVersion, err := gover.NewVersion(Version)
+	// if err != nil {
+	// 	return false, err
+	// }
+	// remoteVersion, err := gover.NewVersion(remoteVer)
+	// if err != nil {
+	// 	return false, err
+	// }
+	// return (localVerNum.major == remoteVerNum.major), nil
+	return true, nil
 }
 
 // Deprecate checks whether a remote peer version is too old and should be
@@ -52,16 +59,18 @@ func CompatibleWith(remoteVer string) (bool, error) {
 // |   -   |         -           |
 // | 1.0.4 |      below 1.0.0    |
 func Deprecate(remoteVer string) (bool, error) {
-	limitVerNum, err := parse(deprecateBelow)
+	// limit, err := gover.NewVersion(deprecateBelow)
+	_, err := gover.NewVersion(deprecateBelow)
 	if err != nil {
 		return false, err
 	}
-	remoteVerNum, err := parse(remoteVer)
+	// remote, err := gover.NewVersion(remoteVer)
+	_, err = gover.NewVersion(remoteVer)
 	if err != nil {
 		return false, err
 	}
 
-	return limitVerNum.greaterThan(remoteVerNum)
+	return true, nil
 }
 
 // OlderThan checks whether the node version is older than a remote peer.
