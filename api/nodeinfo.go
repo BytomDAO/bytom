dbaaad037f29f6ed7fac58dc1a3c7bad00d7e623
HAOYUatHZ <haoyu@protonmail.com>
2018-08-21 15:28:36 +0800

    :hammer: Add lock for version statu

--- api/nodeinfo.go
+++ api/nodeinfo.go
@@ -33,7 +33,7 @@ func (a *API) GetNodeInfo() *NetInfo {
 		CurrentBlock: a.chain.BestBlockHeight(),
 		NetWorkID:    a.sync.NodeInfo().Network,
 		Version:      version.Version,
-		Update:       version.Status,
+		Update:       version.Status.GetVersionStatus(),
 	}
 	if bestPeer := a.sync.BestPeer(); bestPeer != nil {
 		info.HighestBlock = bestPeer.Height
--- version/version.go
+++ version/version.go
@@ -4,28 +4,44 @@ import (
 	gover "github.com/hashicorp/go-version"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/fatih/set.v0"
+	"sync"
 )
 
 const (
 	noUpdate   uint16 = iota
 	hasUpdate  uint16 = iota
-	hasSUpdate uint16 = iota
+	hasMUpdate uint16 = iota
 )
 
 var (
 	// The full version string
-	Version = "1.0.4"
+	Version = "1.0.3"
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
-	notified  bool
 	SeedSet   = set.New()
-	Status    = noUpdate
+	Status    *State
 )
 
 func init() {
 	if GitCommit != "" {
 		Version += "-" + GitCommit[:8]
 	}
+	Status = &State{
+		notified:      false,
+		VersionStatus: noUpdate,
+	}
+}
+
+type State struct {
+	sync.RWMutex
+	notified      bool
+	VersionStatus uint16
+}
+
+func (s *State) GetVersionStatus() uint16 {
+	s.RLock()
+	defer s.RUnlock()
+	return s.VersionStatus
 }
 
 // CheckUpdate checks whether there is a newer version to update.
@@ -38,7 +54,10 @@ func init() {
 // 		1. small update: seed version is higher than the node itself
 // 		2. significant update: seed mojor version is higher than the node itself
 func CheckUpdate(localVerStr string, remoteVerStr string, remoteAddr string) error {
-	if notified || !SeedSet.Has(remoteAddr) {
+	Status.Lock()
+	defer Status.Unlock()
+
+	if Status.notified || !SeedSet.Has(remoteAddr) {
 		return nil
 	}
 
@@ -51,18 +70,18 @@ func CheckUpdate(localVerStr string, remoteVerStr string, remoteAddr string) err
 		return err
 	}
 	if remoteVersion.GreaterThan(localVersion) {
-		Status = hasUpdate
+		Status.VersionStatus = hasUpdate
 	}
 	if remoteVersion.Segments()[0] > localVersion.Segments()[0] {
-		Status = hasSUpdate
+		Status.VersionStatus = hasMUpdate
 	}
-	if Status != noUpdate {
+	if Status.VersionStatus != noUpdate {
 		log.WithFields(log.Fields{
 			"Current version": localVerStr,
 			"Newer version":   remoteVerStr,
 			"seed":            remoteAddr}).
 			Warn("Please update your bytomd via https://github.com/Bytom/bytom/releases/ or http://bytom.io/wallet/")
-		notified = true
+		Status.notified = true
 	}
 	return nil
 }
