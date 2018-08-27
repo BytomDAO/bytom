04b7ee7a42d9cf0866a3fb7b72b50adf8a2be629
Chengcheng Zhang <943420582@qq.com>
2018-08-27 19:35:06 +0800

    add chainkd.go util.go sm2_key.go

---
+++ crypto/sm2/chainkd/chainkd.go
@@ -0,0 +1,8 @@
+package chainkd
+
+type (
+	//XPrv external private key
+	XPrv [64]byte
+	//XPub external public key
+	XPub [64]byte
+)
---
+++ crypto/sm2/chainkd/util.go
@@ -0,0 +1,33 @@
+package chainkd
+
+import (
+	"io"
+)
+
+// Utility functions
+
+// NewXKeys create new xprv and xpub
+func NewXKeys(r io.Reader) (xprv XPrv, xpub XPub, err error) {
+	xprv, err = NewXPrv(r)
+	if err != nil {
+		return
+	}
+	return xprv, xprv.XPub(), nil
+}
+
+// func XPubKeys(xpubs []XPub) []ed25519.PublicKey {
+// 	res := make([]ed25519.PublicKey, 0, len(xpubs))
+// 	for _, xpub := range xpubs {
+// 		res = append(res, xpub.PublicKey())
+// 	}
+// 	return res
+// }
+
+// func DeriveXPubs(xpubs []XPub, path [][]byte) []XPub {
+// 	res := make([]XPub, 0, len(xpubs))
+// 	for _, xpub := range xpubs {
+// 		d := xpub.Derive(path)
+// 		res = append(res, d)
+// 	}
+// 	return res
+// }
