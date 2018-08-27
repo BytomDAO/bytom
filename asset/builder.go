199bcd2b6a89ad363064fa4a2e9bba28f45ade15
oysheng <oysheng@bytom.io>
2018-08-27 14:46:39 +0800

    optimise

--- asset/builder.go
+++ asset/builder.go
@@ -10,14 +10,8 @@ import (
 
 	"github.com/bytom/blockchain/signers"
 	"github.com/bytom/blockchain/txbuilder"
-	"github.com/bytom/errors"
 	"github.com/bytom/protocol/bc"
 	"github.com/bytom/protocol/bc/types"
-)
-
-var (
-	// ErrMissingIssueArgs means missing arguments for issue program
-	ErrMissingIssueArgs = errors.New("missing issue contract arguments")
 )
 
 //NewIssueAction create a new asset issue action
@@ -59,13 +53,10 @@ func (a *issueAction) Build(ctx context.Context, builder *txbuilder.TemplateBuil
 
 	txin := types.NewIssuanceInput(nonce[:], a.Amount, asset.IssuanceProgram, nil, asset.RawDefinitionByte)
 	tplIn := &txbuilder.SigningInstruction{}
-	if a.Arguments == nil {
-		if asset.Signer == nil {
-			return ErrMissingIssueArgs
-		}
+	if asset.Signer != nil {
 		path := signers.Path(asset.Signer, signers.AssetKeySpace)
 		tplIn.AddRawWitnessKeys(asset.Signer.XPubs, path, asset.Signer.Quorum)
-	} else {
+	} else if a.Arguments != nil {
 		if err := txbuilder.AddContractArgs(tplIn, a.Arguments); err != nil {
 			return err
 		}
