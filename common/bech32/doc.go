ae85642268fc000fb1b6befd7d3bd4c759a46674
HAOYUatHZ <haoyu@protonmail.com>
2018-09-01 11:59:22 +0800

    Clean

--- common/bech32/doc.go
+++ common/bech32/doc.go
@@ -1,7 +1,3 @@
-// Copyright (c) 2017 The btcsuite developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
 /*
 Package bech32 provides a Go implementation of the bech32 format specified in
 BIP 173.
