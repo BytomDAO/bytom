060d98c2a153882b1f749b95cc4648dfab764460
HAOYUatHZ <haoyu@protonmail.com>
2018-09-03 20:17:12 +0800

    Avoid rangeValCopy (each iteration copies 200 bytes)

--- crypto/sm2/x509.go
+++ crypto/sm2/x509.go
@@ -1378,7 +1378,8 @@ func parseCertificate(in *certificate) (*Certificate, error) {
 					return nil, errors.New("x509: trailing data after X.509 CRL distribution point")
 				}
 
-				for _, dp := range cdp {
+				for i := range cdp {
+					dp := &cdp[i]
 					// Per RFC 5280, 4.2.1.13, one of distributionPoint or cRLIssuer may be empty.
 					if len(dp.DistributionPoint.FullName.Bytes) == 0 {
 						continue
