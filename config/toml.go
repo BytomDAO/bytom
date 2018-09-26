1a323c4b981a35d95099171d600fa4497df77308
Chengcheng Zhang <943420582@qq.com>
2018-09-26 16:06:03 +0800

    update toml.go

--- config/toml.go
+++ config/toml.go
@@ -21,32 +21,26 @@ func EnsureRoot(rootDir string, network string) {
 
 var defaultConfigTmpl = `# This is a TOML config file.
 # For more information, see https://github.com/toml-lang/toml
+fast_sync = true
+db_backend = "leveldb"
+api_addr = "0.0.0.0:9888"
 `
 
-var mainNetConfigTmpl = `fast_sync = true
-db_backend = "leveldb"
-api_addr = "0.0.0.0:9889"
-chain_id = "mainnet"
+var mainNetConfigTmpl = `chain_id = "mainnet"
 [p2p]
-laddr = "tcp://0.0.0.0:46660"
-seeds = "52.83.251.197:46660"
+laddr = "tcp://0.0.0.0:46657"
+seeds = "139.162.105.40:46657"
 `
 
-var testNetConfigTmpl = `fast_sync = true
-db_backend = "leveldb"
-api_addr = "0.0.0.0:9890"
-chain_id = "wisdom"
+var testNetConfigTmpl = `chain_id = "wisdom"
 [p2p]
-laddr = "tcp://0.0.0.0:46659"
-seeds = "52.83.251.197:46659"
+laddr = "tcp://0.0.0.0:46656"
+seeds = "139.162.105.40:46656"
 `
 
-var soloNetConfigTmpl = `fast_sync = true
-db_backend = "leveldb"
-api_addr = "0.0.0.0:9891"
-chain_id = "solonet"
+var soloNetConfigTmpl = `chain_id = "solonet"
 [p2p]
-laddr = "tcp://0.0.0.0:46661"
+laddr = "tcp://0.0.0.0:46658"
 seeds = ""
 `
 
