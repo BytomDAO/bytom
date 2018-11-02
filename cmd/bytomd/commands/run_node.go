d84cbd73fc318ba8c87dd35e81948df80c952898
shenao78 <shenao.78@163.com>
2018-11-02 14:27:43 +0800

    select utxo parameterize

--- cmd/bytomd/commands/run_node.go
+++ cmd/bytomd/commands/run_node.go
@@ -26,6 +26,7 @@ func init() {
 
 	runNodeCmd.Flags().Bool("wallet.disable", config.Wallet.Disable, "Disable wallet")
 	runNodeCmd.Flags().Bool("wallet.rescan", config.Wallet.Rescan, "Rescan wallet")
+	runNodeCmd.Flags().String("wallet.utxo_select_strategy", config.Wallet.UTXOSelectStrategy, "Set strategy for select utxo")
 	runNodeCmd.Flags().Bool("vault_mode", config.VaultMode, "Run in the offline enviroment")
 	runNodeCmd.Flags().Bool("web.closed", config.Web.Closed, "Lanch web browser or not")
 	runNodeCmd.Flags().String("chain_id", config.ChainID, "Select network type")
