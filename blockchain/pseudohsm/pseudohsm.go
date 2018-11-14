905a025df21b37511ab61c740db80cc4ec19f577
oysheng <33340252+oysheng@users.noreply.github.com>
2018-11-14 11:07:22 +0800

    add update-key-alias (#1460)
    * add update-key-alias
    
    * optimise
    
    * optimise test
    
    * optmise

--- blockchain/pseudohsm/pseudohsm.go
+++ blockchain/pseudohsm/pseudohsm.go
@@ -3,6 +3,8 @@ package pseudohsm
 
 import (
 	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -65,7 +67,7 @@ func (h *HSM) XCreate(alias string, auth string, language string) (*XPub, *strin
 	return xpub, mnemonic, err
 }
 
-// ImportFromMnemonic produces a xprv from mnemonic and stores it in the db.
+// ImportKeyFromMnemonic produces a xprv from mnemonic and stores it in the db.
 func (h *HSM) ImportKeyFromMnemonic(alias string, auth string, mnemonic string, language string) (*XPub, error) {
 	h.cacheMu.Lock()
 	defer h.cacheMu.Unlock()
@@ -127,6 +129,52 @@ func (h *HSM) createChainKDKey(alias string, auth string, language string) (*XPu
 		return nil, nil, err
 	}
 	return xpub, &mnemonic, nil
+}
+
+// UpdateKeyAlias update key alias
+func (h *HSM) UpdateKeyAlias(xpub chainkd.XPub, newAlias string) error {
+	h.cacheMu.Lock()
+	defer h.cacheMu.Unlock()
+
+	h.cache.maybeReload()
+	h.cache.mu.Lock()
+	xpb, err := h.cache.find(XPub{XPub: xpub})
+	h.cache.mu.Unlock()
+	if err != nil {
+		return err
+	}
+
+	keyjson, err := ioutil.ReadFile(xpb.File)
+	if err != nil {
+		return err
+	}
+
+	encrptKeyJSON := new(encryptedKeyJSON)
+	if err := json.Unmarshal(keyjson, encrptKeyJSON); err != nil {
+		return err
+	}
+
+	normalizedAlias := strings.ToLower(strings.TrimSpace(newAlias))
+	if ok := h.cache.hasAlias(normalizedAlias); ok {
+		return ErrDuplicateKeyAlias
+	}
+
+	encrptKeyJSON.Alias = normalizedAlias
+	keyJSON, err := json.Marshal(encrptKeyJSON)
+	if err != nil {
+		return err
+	}
+
+	if err := writeKeyFile(xpb.File, keyJSON); err != nil {
+		return err
+	}
+
+	// update key alias
+	h.cache.delete(xpb)
+	xpb.Alias = normalizedAlias
+	h.cache.add(xpb)
+
+	return nil
 }
 
 // ListKeys returns a list of all xpubs from the store
