578090a81a07f25ecf70c23f3327a5ed3b84e2a4
yahtoo <yahtoo.ma@gmail.com>
2019-02-13 17:41:01 +0800

    p2p: support dns seed (#1554)
    * p2p: support dns seed
    
    * Del unused code
    
    * Add test file
    
    * Add multiple dns server parallel support
    
    * Fix review bugs
    
    * Fix review bugs

--- netsync/handle.go
+++ netsync/handle.go
@@ -498,16 +498,24 @@ func initDiscover(config *cfg.Config, priv *crypto.PrivKeyEd25519, port uint16) 
 		return nil, err
 	}
 
-	// add the seeds node to the discover table
-	if config.P2P.Seeds == "" {
+	seeds, err := p2p.QueryDNSSeeds(net.LookupHost)
+	if err != nil {
+		log.WithFields(log.Fields{"module": logModule, "err": err}).Error("fail on query dns seeds")
+	}
+
+	seeds = append(seeds, strings.Split(config.P2P.Seeds, ",")...)
+
+	if len(seeds) == 0 {
 		return ntab, nil
 	}
-	nodes := []*discover.Node{}
-	for _, seed := range strings.Split(config.P2P.Seeds, ",") {
+
+	var nodes []*discover.Node
+	for _, seed := range seeds {
 		version.Status.AddSeed(seed)
 		url := "enode://" + hex.EncodeToString(crypto.Sha256([]byte(seed))) + "@" + seed
 		nodes = append(nodes, discover.MustParseNode(url))
 	}
+
 	if err = ntab.SetFallbackNodes(nodes); err != nil {
 		return nil, err
 	}
