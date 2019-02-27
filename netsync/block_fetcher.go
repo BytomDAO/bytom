cc515c20f56bc7d7b564ed02516953616a4be102
Yahtoo Ma <yahtoo.ma@gmail.com>
2019-02-27 17:25:05 +0800

    fix review bug

--- netsync/block_fetcher.go
+++ netsync/block_fetcher.go
@@ -92,17 +92,6 @@ func (f *blockFetcher) insert(msg *blockMsg) {
 		log.WithFields(log.Fields{"module": logModule, "err": err}).Error("blockFetcher fail on broadcast new block")
 		return
 	}
-
-	genesisBlock, err := f.chain.GetBlockByHeight(0)
-	if err != nil {
-		log.WithFields(log.Fields{"module": logModule, "err": err}).Error("fail on handleStatusRequestMsg get genesis")
-		return
-	}
-
-	if err = f.peers.broadcastNewStatus(msg.block, genesisBlock); err != nil {
-		log.WithFields(log.Fields{"module": logModule, "err": err}).Error("fail on syncWorker broadcast new status")
-	}
-
 }
 
 func (f *blockFetcher) processNewBlock(msg *blockMsg) {
