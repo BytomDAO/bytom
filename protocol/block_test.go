b70435942b2a13ddf220f884c631277bf6a2c36b
oysheng <oysheng@bytom.io>
2019-03-26 11:02:59 +0800

    add edge test for CalcReorganizeNodes

--- protocol/block_test.go
+++ protocol/block_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/bytom/config"
+	"github.com/bytom/protocol/bc"
 	"github.com/bytom/protocol/state"
 	"github.com/bytom/testutil"
 )
@@ -52,5 +53,85 @@ func TestCalcReorganizeNodes(t *testing.T) {
 	}
 	if !testutil.DeepEqual(wantDetachNodes, getDetachNodes) {
 		t.Errorf("detach nodes want %v but get %v", wantDetachNodes, getDetachNodes)
+	}
+}
+
+func TestEdgeCalcReorganizeNodes(t *testing.T) {
+	header := config.GenesisBlock().BlockHeader
+	initNode, err := state.NewBlockNode(&header, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testNodes := []*state.BlockNode{initNode}
+	testNewNodes := []*state.BlockNode{initNode}
+	for i := uint64(1); i <= 5; i++ {
+		node := &state.BlockNode{
+			Height: i,
+			Nonce:  0,
+			Hash:   bc.Hash{V0: uint64(i)},
+			Parent: testNodes[i-1],
+		}
+		testNodes = append(testNodes, node)
+		node.Nonce = i
+		node.Hash =  bc.Hash{V1: uint64(i)}
+		testNewNodes = append(testNewNodes, node)
+	}
+
+	cases := []struct {
+		mainChainNode   *state.BlockNode
+		newNode         *state.BlockNode
+		wantAttachNodes []*state.BlockNode
+		wantDetachNodes []*state.BlockNode
+	}{
+		{
+			mainChainNode:   testNodes[1],
+			newNode:         testNodes[5],
+			wantAttachNodes: testNodes[2:],
+			wantDetachNodes: []*state.BlockNode{},
+		},
+		{
+			mainChainNode:   testNodes[5],
+			newNode:         testNodes[2],
+			wantAttachNodes: []*state.BlockNode{},
+			wantDetachNodes: []*state.BlockNode{testNodes[5], testNodes[4], testNodes[3]},
+		},
+		{
+			mainChainNode:   testNodes[2],
+			newNode:         testNodes[2],
+			wantAttachNodes: []*state.BlockNode{},
+			wantDetachNodes: []*state.BlockNode{},
+		},
+		{
+			mainChainNode:   testNewNodes[3],
+			newNode:         testNodes[2],
+			wantAttachNodes: []*state.BlockNode{},
+			wantDetachNodes: []*state.BlockNode{testNewNodes[3]},
+		},
+		{
+			mainChainNode:   testNewNodes[2],
+			newNode:         testNodes[3],
+			wantAttachNodes: []*state.BlockNode{testNewNodes[3]},
+			wantDetachNodes: []*state.BlockNode{},
+		},
+	}
+
+	for _, c := range cases {
+		chain := &Chain{index: state.NewBlockIndex()}
+		chain.index.AddNode(initNode)
+		for i := uint64(1); i <= c.mainChainNode.Height; i++ {
+			chain.index.AddNode(testNodes[i])
+		}
+		chain.bestNode = c.mainChainNode
+		chain.index.SetMainChain(c.mainChainNode)
+		getAttachNodes, getDetachNodes := chain.calcReorganizeNodes(c.newNode)
+
+		if !testutil.DeepEqual(c.wantAttachNodes, getAttachNodes) {
+			t.Errorf("attach nodes want %v but get %v", c.wantAttachNodes, getAttachNodes)
+		}
+
+		if !testutil.DeepEqual(c.wantDetachNodes, getDetachNodes) {
+			t.Errorf("detach nodes want %v but get %v", c.wantDetachNodes, getDetachNodes)
+		}
 	}
 }
