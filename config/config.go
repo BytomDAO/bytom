ad5948b4ff3734e5723d4c28bf24199e2c4f1415
HAOYUatHZ <haoyu@protonmail.com>
2019-03-27 14:41:09 +0800

    feat: init keep_connect & inbound_white_list params for p2p config

--- config/config.go
+++ config/config.go
@@ -151,6 +151,8 @@ type P2PConfig struct {
 	ProxyAddress     string `mapstructure:"proxy_address"`
 	ProxyUsername    string `mapstructure:"proxy_username"`
 	ProxyPassword    string `mapstructure:"proxy_password"`
+	KeepConnect      string `mapstructure:"keep_connect"`
+	InboundWhiteList string `mapstructure:"inbound_white_list"`
 }
 
 // Default configurable p2p parameters.
