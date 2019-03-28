b3ac2d9d74dea40888f40e2525730ae520633623
Yahtoo Ma <yahtoo.ma@gmail.com>
2019-03-28 15:34:04 +0800

    Add TestSetMainChainExtendCap test case `

--- protocol/state/blockindex_test.go
+++ protocol/state/blockindex_test.go
@@ -4,6 +4,8 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
+
 	"github.com/bytom/consensus"
 	"github.com/bytom/consensus/difficulty"
 	"github.com/bytom/protocol/bc"
@@ -43,7 +45,7 @@ func TestCalcPastMedianTime(t *testing.T) {
 
 	for idx, c := range cases {
 		var parentNode *BlockNode
-		for i, _ := range c.Timestamps {
+		for i := range c.Timestamps {
 			blockHeader := &types.BlockHeader{
 				Height:    uint64(i),
 				Timestamp: c.Timestamps[i],
@@ -208,5 +210,66 @@ func TestSetMainChain(t *testing.T) {
 			t.Fatalf("old chain block %d, hash %v still in main chain", tailNode.Height, tailNode.Hash)
 		}
 		tailNode = tailNode.Parent
+	}
+}
+
+// MockBlockIndex will mock a empty BlockIndex
+func MockBlockIndex() *BlockIndex {
+	return &BlockIndex{
+		index:     make(map[bc.Hash]*BlockNode),
+		mainChain: make([]*BlockNode, 0, 2),
+	}
+}
+
+func TestSetMainChainExtendCap(t *testing.T) {
+	blockIndex := MockBlockIndex()
+	var lastNode *BlockNode
+
+	cases := []struct {
+		start   uint64
+		stop    uint64
+		wantLen int
+		wantCap int
+	}{
+		{
+			start:   0,
+			stop:    500,
+			wantLen: 500,
+			wantCap: 500 + approxNodesPerDay,
+		},
+		{
+			start:   500,
+			stop:    1000,
+			wantLen: 1000,
+			wantCap: 500 + approxNodesPerDay,
+		},
+		{
+			start:   1000,
+			stop:    2000,
+			wantLen: 2000,
+			wantCap: 2000 + approxNodesPerDay,
+		},
+	}
+
+	for num, c := range cases {
+		for i := c.start; i < c.stop; i++ {
+			node := &BlockNode{
+				Height: i,
+				Hash:   bc.Hash{V0: i},
+				Parent: lastNode,
+			}
+			blockIndex.AddNode(node)
+			lastNode = node
+		}
+		blockIndex.SetMainChain(lastNode)
+		if c.wantLen != len(blockIndex.mainChain) || c.wantCap != cap(blockIndex.mainChain) {
+			t.Fatalf("SetMainChain extended capacity error, index: %d, got len: %d, got cap: %d, want len: %d, want cap: %d", num, len(blockIndex.mainChain), cap(blockIndex.mainChain), c.wantLen, c.wantCap)
+		}
+	}
+
+	for i := 0; i < len(blockIndex.mainChain); i++ {
+		if blockIndex.mainChain[i] != blockIndex.index[blockIndex.mainChain[i].Hash] {
+			t.Fatal("SetMainChain extended capacity error, index:", i, "want:", spew.Sdump(blockIndex.mainChain[i]), "got:", spew.Sdump(blockIndex.index[blockIndex.mainChain[i].Hash]))
+		}
 	}
 }
