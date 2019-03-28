e6ee95134fc44deabe026f2adf526ab012807196
shenao78 <shenao.78@163.com>
2019-03-28 14:32:46 +0800

    replace goleveldb with tendermint db

--- p2p/discover/database.go
+++ p2p/discover/database.go
@@ -7,21 +7,21 @@ import (
 	"bytes"
 	"crypto/rand"
 	"encoding/binary"
-	"fmt"
 	"os"
 	"sync"
 	"time"
-
 	log "github.com/sirupsen/logrus"
-	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/errors"
-	"github.com/syndtr/goleveldb/leveldb/iterator"
-	"github.com/syndtr/goleveldb/leveldb/opt"
-	"github.com/syndtr/goleveldb/leveldb/storage"
-	"github.com/syndtr/goleveldb/leveldb/util"
+	dbm "github.com/tendermint/tmlibs/db"
+	//"github.com/syndtr/goleveldb/leveldb"
+	//"github.com/syndtr/goleveldb/leveldb/errors"
+	//"github.com/syndtr/goleveldb/leveldb/iterator"
+	//"github.com/syndtr/goleveldb/leveldb/opt"
+	//"github.com/syndtr/goleveldb/leveldb/storage"
+	//"github.com/syndtr/goleveldb/leveldb/util"
 	"github.com/tendermint/go-wire"
-
 	"github.com/bytom/crypto"
+	"path"
+	"strconv"
 )
 
 var (
@@ -32,7 +32,7 @@ var (
 
 // nodeDB stores all nodes we know about.
 type nodeDB struct {
-	lvl    *leveldb.DB   // Interface to the database itself
+	lvl    dbm.DB   // Interface to the database itself
 	self   NodeID        // Own node id to prevent adding it into the database
 	runner sync.Once     // Ensures we can start at most one expirer
 	quit   chan struct{} // Channel to signal the expiring thread to stop
@@ -63,10 +63,7 @@ func newNodeDB(path string, version int, self NodeID) (*nodeDB, error) {
 // newMemoryNodeDB creates a new in-memory node database without a persistent
 // backend.
 func newMemoryNodeDB(self NodeID) (*nodeDB, error) {
-	db, err := leveldb.Open(storage.NewMemStorage(), nil)
-	if err != nil {
-		return nil, err
-	}
+	db := dbm.NewMemDB()
 	return &nodeDB{
 		lvl:  db,
 		self: self,
@@ -76,43 +73,31 @@ func newMemoryNodeDB(self NodeID) (*nodeDB, error) {
 
 // newPersistentNodeDB creates/opens a leveldb backed persistent node database,
 // also flushing its contents in case of a version mismatch.
-func newPersistentNodeDB(path string, version int, self NodeID) (*nodeDB, error) {
-	opts := &opt.Options{OpenFilesCacheCapacity: 5}
-	db, err := leveldb.OpenFile(path, opts)
-	if _, iscorrupted := err.(*errors.ErrCorrupted); iscorrupted {
-		db, err = leveldb.RecoverFile(path, nil)
+func newPersistentNodeDB(filePath string, version int, self NodeID) (*nodeDB, error) {
+	dir, file := path.Split(filePath)
+	if file == "" {
+		file = strconv.FormatInt(time.Now().Unix(), 10)
 	}
-	if err != nil {
-		return nil, err
-	}
+	db := dbm.NewDB(file, dbm.GoLevelDBBackendStr, dir)
+
 	// The nodes contained in the cache correspond to a certain protocol version.
 	// Flush all nodes if the version doesn't match.
 	currentVer := make([]byte, binary.MaxVarintLen64)
 	currentVer = currentVer[:binary.PutVarint(currentVer, int64(version))]
 
-	blob, err := db.Get(nodeDBVersionKey, nil)
-	switch err {
-	case leveldb.ErrNotFound:
-		// Version not found (i.e. empty cache), insert it
-		if err = db.Put(nodeDBVersionKey, currentVer, nil); err != nil {
-			if err := db.Close(); err != nil {
-				log.WithFields(log.Fields{"module": logModule, "error": err}).Warn(fmt.Sprintf("db close err"))
-			}
-			return nil, err
-		}
-
-	case nil:
-		// Version present, flush if different
+	blob := db.Get(nodeDBVersionKey)
+	if blob == nil {
+		db.Set(nodeDBVersionKey, currentVer)
+	} else {
 		if !bytes.Equal(blob, currentVer) {
-			if err = db.Close(); err != nil {
-				log.WithFields(log.Fields{"module": logModule, "error": err}).Warn(fmt.Sprintf("db close err"))
-			}
-			if err = os.RemoveAll(path); err != nil {
+			db.Close()
+			if err := os.RemoveAll(filePath + ".db"); err != nil {
 				return nil, err
 			}
-			return newPersistentNodeDB(path, version, self)
+			return newPersistentNodeDB(filePath, version, self)
 		}
 	}
+
 	return &nodeDB{
 		lvl:  db,
 		self: self,
@@ -146,8 +131,8 @@ func splitKey(key []byte) (id NodeID, field string) {
 // fetchInt64 retrieves an integer instance associated with a particular
 // database key.
 func (db *nodeDB) fetchInt64(key []byte) int64 {
-	blob, err := db.lvl.Get(key, nil)
-	if err != nil {
+	blob := db.lvl.Get(key)
+	if blob == nil {
 		return 0
 	}
 	val, read := binary.Varint(blob)
@@ -159,10 +144,10 @@ func (db *nodeDB) fetchInt64(key []byte) int64 {
 
 // storeInt64 update a specific database entry to the current time instance as a
 // unix timestamp.
-func (db *nodeDB) storeInt64(key []byte, n int64) error {
+func (db *nodeDB) storeInt64(key []byte, n int64) {
 	blob := make([]byte, binary.MaxVarintLen64)
 	blob = blob[:binary.PutVarint(blob, n)]
-	return db.lvl.Put(key, blob, nil)
+	db.lvl.Set(key, blob)
 }
 
 // node retrieves a node with a given id from the database.
@@ -173,12 +158,9 @@ func (db *nodeDB) node(id NodeID) *Node {
 	)
 
 	key := makeKey(id, nodeDBDiscoverRoot)
-	rawData, err := db.lvl.Get(key, nil)
-	if err != nil {
-		log.WithFields(log.Fields{"module": logModule, "error": err}).Warn(fmt.Sprintf("get node rawdata err"))
-		return nil
-	}
+	rawData := db.lvl.Get(key)
 
+	var err error
 	wire.ReadBinary(node, bytes.NewReader(rawData), 0, &n, &err)
 	if err != nil {
 		log.WithFields(log.Fields{"module": logModule, "key": key, "node": node, "error": err}).Warn("get node from db err")
@@ -202,18 +184,16 @@ func (db *nodeDB) updateNode(node *Node) error {
 		return err
 	}
 
-	return db.lvl.Put(makeKey(node.ID, nodeDBDiscoverRoot), blob.Bytes(), nil)
+	db.lvl.Set(makeKey(node.ID, nodeDBDiscoverRoot), blob.Bytes())
+	return nil
 }
 
 // deleteNode deletes all information/keys associated with a node.
-func (db *nodeDB) deleteNode(id NodeID) error {
-	deleter := db.lvl.NewIterator(util.BytesPrefix(makeKey(id, "")), nil)
+func (db *nodeDB) deleteNode(id NodeID) {
+	deleter := db.lvl.IteratorPrefix(makeKey(id, ""))
 	for deleter.Next() {
-		if err := db.lvl.Delete(deleter.Key(), nil); err != nil {
-			return err
-		}
+		db.lvl.Delete(deleter.Key())
 	}
-	return nil
 }
 
 // ensureExpirer is a small helper method ensuring that the data expiration
@@ -252,7 +232,7 @@ func (db *nodeDB) expireNodes() error {
 	threshold := time.Now().Add(-nodeDBNodeExpiration)
 
 	// Find discovered nodes that are older than the allowance
-	it := db.lvl.NewIterator(nil, nil)
+	it := db.lvl.Iterator()
 	defer it.Release()
 
 	for it.Next() {
@@ -268,9 +248,7 @@ func (db *nodeDB) expireNodes() error {
 			}
 		}
 		// Otherwise delete all associated information
-		if err := db.deleteNode(id); err != nil {
-			return err
-		}
+		db.deleteNode(id)
 	}
 
 	return nil
@@ -283,8 +261,8 @@ func (db *nodeDB) lastPing(id NodeID) time.Time {
 }
 
 // updateLastPing updates the last time we tried contacting a remote node.
-func (db *nodeDB) updateLastPing(id NodeID, instance time.Time) error {
-	return db.storeInt64(makeKey(id, nodeDBDiscoverPing), instance.Unix())
+func (db *nodeDB) updateLastPing(id NodeID, instance time.Time) {
+	db.storeInt64(makeKey(id, nodeDBDiscoverPing), instance.Unix())
 }
 
 // lastPong retrieves the time of the last successful contact from remote node.
@@ -293,8 +271,8 @@ func (db *nodeDB) lastPong(id NodeID) time.Time {
 }
 
 // updateLastPong updates the last time a remote node successfully contacted.
-func (db *nodeDB) updateLastPong(id NodeID, instance time.Time) error {
-	return db.storeInt64(makeKey(id, nodeDBDiscoverPong), instance.Unix())
+func (db *nodeDB) updateLastPong(id NodeID, instance time.Time) {
+	db.storeInt64(makeKey(id, nodeDBDiscoverPong), instance.Unix())
 }
 
 // findFails retrieves the number of findnode failures since bonding.
@@ -303,8 +281,8 @@ func (db *nodeDB) findFails(id NodeID) int {
 }
 
 // updateFindFails updates the number of findnode failures since bonding.
-func (db *nodeDB) updateFindFails(id NodeID, fails int) error {
-	return db.storeInt64(makeKey(id, nodeDBDiscoverFindFails), int64(fails))
+func (db *nodeDB) updateFindFails(id NodeID, fails int) {
+	db.storeInt64(makeKey(id, nodeDBDiscoverFindFails), int64(fails))
 }
 
 // querySeeds retrieves random nodes to be used as potential seed nodes
@@ -313,7 +291,7 @@ func (db *nodeDB) querySeeds(n int, maxAge time.Duration) []*Node {
 	var (
 		now   = time.Now()
 		nodes = make([]*Node, 0, n)
-		it    = db.lvl.NewIterator(nil, nil)
+		it    = db.lvl.Iterator()
 		id    NodeID
 	)
 	defer it.Release()
@@ -328,7 +306,7 @@ seek:
 			log.WithFields(log.Fields{"module": logModule, "error": err}).Warn("get rand date")
 		}
 		id[0] = ctr + id[0]%16
-		it.Seek(makeKey(id, nodeDBDiscoverRoot))
+		it = db.lvl.IteratorPrefix(makeKey(id, nodeDBDiscoverRoot))
 
 		n := nextNode(it)
 		if n == nil {
@@ -353,10 +331,7 @@ seek:
 
 func (db *nodeDB) fetchTopicRegTickets(id NodeID) (issued, used uint32) {
 	key := makeKey(id, nodeDBTopicRegTickets)
-	blob, err := db.lvl.Get(key, nil)
-	if err != nil {
-		log.WithFields(log.Fields{"module": logModule, "error": err}).Warn("db get raw data")
-	}
+	blob := db.lvl.Get(key)
 
 	if len(blob) != 8 {
 		return 0, 0
@@ -366,24 +341,24 @@ func (db *nodeDB) fetchTopicRegTickets(id NodeID) (issued, used uint32) {
 	return
 }
 
-func (db *nodeDB) updateTopicRegTickets(id NodeID, issued, used uint32) error {
+func (db *nodeDB) updateTopicRegTickets(id NodeID, issued, used uint32) {
 	key := makeKey(id, nodeDBTopicRegTickets)
 	blob := make([]byte, 8)
 	binary.BigEndian.PutUint32(blob[0:4], issued)
 	binary.BigEndian.PutUint32(blob[4:8], used)
-	return db.lvl.Put(key, blob, nil)
+	db.lvl.Set(key, blob)
 }
 
 // reads the next node record from the iterator, skipping over other
 // database entries.
-func nextNode(it iterator.Iterator) *Node {
+func nextNode(it dbm.Iterator) *Node {
 	var (
 		n    = int(0)
 		err  = error(nil)
 		node = new(Node)
 	)
 
-	for end := false; !end; end = !it.Next() {
+	for end := !it.Next(); !end; end = !it.Next() {
 		id, field := splitKey(it.Key())
 		if field != nodeDBDiscoverRoot {
 			continue
@@ -403,7 +378,5 @@ func nextNode(it iterator.Iterator) *Node {
 // close flushes and closes the database files.
 func (db *nodeDB) close() {
 	close(db.quit)
-	if err := db.lvl.Close(); err != nil {
-		log.WithFields(log.Fields{"module": logModule, "error": err}).Warn("db close err")
-	}
+	db.lvl.Close()
 }
