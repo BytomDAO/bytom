a6f5816810ad1711f156bc782e2adc55279c99e5
paladz <453256728@qq.com>
2019-04-18 22:10:35 +0800

    delete test data

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -24,26 +24,6 @@ import (
 	"github.com/bytom/protocol/bc/types"
 )
 
-func TestEncodeDecodeGlobalTxIndex(t *testing.T) {
-	want := &struct {
-		BlockHash bc.Hash
-		Position  uint64
-	}{
-		BlockHash: bc.NewHash([32]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20}),
-		Position:  1,
-	}
-
-	globalTxIdx := calcGlobalTxIndex(&want.BlockHash, want.Position)
-	blockHashGot, positionGot := parseGlobalTxIdx(globalTxIdx)
-	if *blockHashGot != want.BlockHash {
-		t.Errorf("blockHash mismatch. Get: %v. Expect: %v", *blockHashGot, want.BlockHash)
-	}
-
-	if positionGot != want.Position {
-		t.Errorf("position mismatch. Get: %v. Expect: %v", positionGot, want.Position)
-	}
-}
-
 func TestWalletVersion(t *testing.T) {
 	// prepare wallet
 	dirPath, err := ioutil.TempDir(".", "")
@@ -189,15 +169,6 @@ func TestWalletUpdate(t *testing.T) {
 
 	if wants[0].ID != tx.ID {
 		t.Fatal("account txID mismatch")
-	}
-
-	for position, tx := range block.Transactions {
-		get := w.DB.Get(calcGlobalTxIndexKey(tx.ID.String()))
-		bh := block.BlockHeader.Hash()
-		expect := calcGlobalTxIndex(&bh, uint64(position))
-		if !reflect.DeepEqual(get, expect) {
-			t.Fatalf("position#%d: compare retrieved globalTxIdx err", position)
-		}
 	}
 }
 
