e783add75f0b225cab1323ab88737c2cbc25c92f
Poseidon <shenao.78@163.com>
2021-04-07 21:45:23 +0800

    add_casper_process_block (#1868)

--- database/store.go
+++ database/store.go
@@ -223,3 +223,12 @@ func (s *Store) SaveChainStatus(node *state.BlockNode, view *state.UtxoViewpoint
 	batch.Write()
 	return nil
 }
+
+func (s *Store) GetCheckpoint(*bc.Hash) (*state.Checkpoint, error) {
+	return nil, nil
+}
+
+// GetCheckpointsByHeight return all checkpoints of specified block height
+func (s *Store) GetCheckpointsByHeight(uint64) ([]*state.Checkpoint, error) {
+	return nil, nil
+}
