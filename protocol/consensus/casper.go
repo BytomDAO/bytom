ff349ba39c0acf16be624f87a9878772aea5f24f
shenao78 <shenao.78@163.com>
2021-04-07 17:08:20 +0800

    add_casper_process_block

--- protocol/consensus/casper.go
+++ protocol/consensus/casper.go
@@ -7,11 +7,13 @@ import (
 	"github.com/bytom/bytom/protocol"
 	"github.com/bytom/bytom/protocol/bc"
 	"github.com/bytom/bytom/protocol/bc/types"
+	"github.com/bytom/bytom/protocol/state"
 )
 
 var (
 	errVerifySignature          = errors.New("signature of verification message is invalid")
 	errPubKeyIsNotValidator     = errors.New("pub key is not in validators of target checkpoint")
+	errVoteToGrowingCheckpoint  = errors.New("validator publish vote to growing checkpoint")
 	errSameHeightInVerification = errors.New("validator publish two distinct votes for the same target height")
 	errSpanHeightInVerification = errors.New("validator publish vote within the span of its other votes")
 )
@@ -25,19 +27,19 @@ type Casper struct {
 }
 
 // Best chain return the chain containing the justified checkpoint of the largest height
-func (c *Casper) BestChain() (uint64, string) {
+func (c *Casper) BestChain() (uint64, bc.Hash) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
 	// root is init justified
 	root := c.tree.checkpoint
-	bestHeight, bestHash, _ := chainOfMaxJustifiedHeight(c.tree, root.height)
+	bestHeight, bestHash, _ := chainOfMaxJustifiedHeight(c.tree, root.Height)
 	return bestHeight, bestHash
 }
 
 // Validators return the validators by specified block hash
 // e.g. if the block num of epoch is 100, and the block height corresponding to the block hash is 130, then will return the voting results of height in 0~100
-func (c *Casper) Validators(blockHash *bc.Hash) ([]*Validator, error) {
+func (c *Casper) Validators(blockHash *bc.Hash) ([]*state.Validator, error) {
 	hash, err := c.prevCheckpointHash(blockHash)
 	if err != nil {
 		return nil, err
@@ -46,12 +48,12 @@ func (c *Casper) Validators(blockHash *bc.Hash) ([]*Validator, error) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
-	checkpoint, err := c.tree.checkpointByHash(hash.String())
+	checkpoint, err := c.store.GetCheckpoint(hash)
 	if err != nil {
 		return nil, err
 	}
 
-	return checkpoint.validators(), nil
+	return checkpoint.Validators(), nil
 }
 
 // AuthVerification verify whether the Verification is legal.
@@ -63,23 +65,25 @@ func (c *Casper) AuthVerification(v *Verification) error {
 		return err
 	}
 
+	source, err := c.store.GetCheckpoint(&v.SourceHash)
+	if err != nil {
+		// the synchronization block is later than the arrival of the verification message
+		return err
+	}
+
+	target, err := c.store.GetCheckpoint(&v.TargetHash)
+	if err != nil {
+		return err
+	}
+
+	if source.Status == state.Growing || target.Status == state.Growing {
+		return errVoteToGrowingCheckpoint
+	}
+
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	source, err := c.tree.checkpointByHash(v.SourceHash)
-	if err != nil {
-		// the following two cases are not handled
-		// case1: the synchronization block is later than the arrival of the verification message
-		// case2: the tree node was was pruned
-		return err
-	}
-
-	target, err := c.tree.checkpointByHash(v.TargetHash)
-	if err != nil {
-		return err
-	}
-
-	if !target.containsValidator(v.PubKey) {
+	if !target.ContainsValidator(v.PubKey) {
 		return errPubKeyIsNotValidator
 	}
 
@@ -91,29 +95,77 @@ func (c *Casper) AuthVerification(v *Verification) error {
 		return err
 	}
 
-	supLink := target.addSupLink(v.SourceHeight, v.SourceHash, v.PubKey)
-	if source.status == justified && supLink.confirmed() {
-		target.status = justified
-		source.status = finalized
+	supLink := target.AddSupLink(v.SourceHeight, v.SourceHash, v.PubKey)
+	if source.Status == state.Justified && supLink.Confirmed() {
+		target.Status = state.Justified
+		source.Status = state.Finalized
 		// must notify chain when rollback
-		// pruning the tree
+		if err := c.pruneTree(source.Hash); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+func (c *Casper) pruneTree(rootHash bc.Hash) error {
+	newRoot, err := c.tree.nodeByHash(rootHash)
+	if err != nil {
+		return err
+	}
+
+	c.tree = newRoot
 	return nil
 }
 
 // ProcessBlock used to receive a new block from upper layer, it provides idempotence
 // and parse the vote and mortgage from the transactions, then save to the checkpoint
 // the tree of checkpoint will grow with the arrival of new blocks
-func (c *Casper) ProcessBlock(block *types.Block) error {
-	return nil
+func (c *Casper) ProcessBlock(block *types.Block) (*state.Checkpoint, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	prevHash := block.PreviousBlockHash
+	node, err := c.tree.nodeByHash(prevHash)
+	if err != nil {
+		return nil, err
+	}
+
+	checkpoint := node.checkpoint
+
+	if mod := block.Height % state.BlocksOfEpoch; mod == 1 {
+		parent := checkpoint
+
+		checkpoint = &state.Checkpoint{
+			PrevHash:       parent.Hash,
+			StartTimestamp: block.Timestamp,
+			Status:         state.Growing,
+			Votes:          make(map[string]uint64),
+			Mortgages:      make(map[string]uint64),
+		}
+		node.children = append(node.children, &treeNode{checkpoint: checkpoint})
+	} else if mod == 0 {
+		checkpoint.Status = state.Unverified
+	}
+
+	checkpoint.Height = block.Height
+	checkpoint.Hash = block.Hash()
+
+	for range block.Transactions {
+		// process the votes and mortgages
+	}
+	return checkpoint, nil
 }
 
 // a validator must not publish two distinct votes for the same target height
 func (c *Casper) verifySameHeight(v *Verification) error {
-	nodes := c.tree.checkpointsOfHeight(v.TargetHeight)
-	for _, node := range nodes {
-		for _, supLink := range node.supLinks {
-			if supLink.pubKeys[v.PubKey] {
+	checkpoints, err := c.store.GetCheckpointsByHeight(v.TargetHeight)
+	if err != nil {
+		return err
+	}
+
+	for _, checkpoint := range checkpoints {
+		for _, supLink := range checkpoint.SupLinks {
+			if supLink.PubKeys[v.PubKey] {
 				return errSameHeightInVerification
 			}
 		}
@@ -123,13 +175,14 @@ func (c *Casper) verifySameHeight(v *Verification) error {
 
 // a validator must not vote within the span of its other votes.
 func (c *Casper) verifySpanHeight(v *Verification) error {
-	if c.tree.findOnlyOne(func(c *checkpoint) bool {
-		if c.height <= v.TargetHeight {
+	// It is best to scan all checkpoints from leveldb
+	if c.tree.findOnlyOne(func(c *state.Checkpoint) bool {
+		if c.Height <= v.TargetHeight {
 			return false
 		}
 
-		for _, supLink := range c.supLinks {
-			if supLink.pubKeys[v.PubKey] && supLink.sourceHeight < v.SourceHeight {
+		for _, supLink := range c.SupLinks {
+			if supLink.PubKeys[v.PubKey] && supLink.SourceHeight < v.SourceHeight {
 				return true
 			}
 		}
@@ -141,13 +194,13 @@ func (c *Casper) verifySpanHeight(v *Verification) error {
 }
 
 // justifiedHeight is the max justified height of checkpoint from node to root
-func chainOfMaxJustifiedHeight(node *treeNode, justifiedHeight uint64) (uint64, string, uint64) {
+func chainOfMaxJustifiedHeight(node *treeNode, justifiedHeight uint64) (uint64, bc.Hash, uint64) {
 	checkpoint := node.checkpoint
-	if checkpoint.status == justified || checkpoint.status == finalized {
-		justifiedHeight = checkpoint.height
+	if checkpoint.Status == state.Justified || checkpoint.Status == state.Finalized {
+		justifiedHeight = checkpoint.Height
 	}
 
-	bestHeight, bestHash, maxJustifiedHeight := checkpoint.height, checkpoint.hash, justifiedHeight
+	bestHeight, bestHash, maxJustifiedHeight := checkpoint.Height, checkpoint.Hash, justifiedHeight
 	for _, child := range node.children {
 		if height, hash, justified := chainOfMaxJustifiedHeight(child, justifiedHeight); justified > maxJustifiedHeight {
 			bestHeight, bestHash, maxJustifiedHeight = height, hash, justified
@@ -165,7 +218,7 @@ func (c *Casper) prevCheckpointHash(blockHash *bc.Hash) (*bc.Hash, error) {
 
 		height := block.Height - 1
 		blockHash = &block.PreviousBlockHash
-		if height%blocksOfEpoch == 0 {
+		if height%state.BlocksOfEpoch == 0 {
 			return blockHash, nil
 		}
 	}
