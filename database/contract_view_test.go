04afebb8df16774ab9d9ebf44b7598c3fa4b4019
DeKaiju <longjinglv@gmail.com>
2021-04-13 14:25:14 +0800

    refactor(bcrp): code refactoring

--- database/contract_view_test.go
+++ database/contract_view_test.go
@@ -15,18 +15,14 @@ import (
 )
 
 var (
-	program []byte
-	hash    [32]byte
-	txID1   *bc.Hash
-	txID2   *bc.Hash
+	program, _ = hex.DecodeString("6a4c04626372704c01014c2820e9108d3ca8049800727f6a3505b3a2710dc579405dde03c250f16d9a7e1e6e787403ae7cac00c0")
+	hash       [32]byte
+	txID1      = &bc.Hash{V0: 1, V1: 1, V2: 1, V3: 1}
+	txID2      = &bc.Hash{V0: 2, V1: 2, V2: 2, V3: 2}
 )
 
 func init() {
-	contract := "6a4c04626372704c01014c2820e9108d3ca8049800727f6a3505b3a2710dc579405dde03c250f16d9a7e1e6e787403ae7cac00c0"
-	program, _ = hex.DecodeString(contract)
 	sha3pool.Sum256(hash[:], program)
-	txID1 = &bc.Hash{V0: 1, V1: 1, V2: 1, V3: 1}
-	txID2 = &bc.Hash{V0: 2, V1: 2, V2: 2, V3: 2}
 }
 
 // register contract by transaction 1
