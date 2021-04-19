9308110966704412890100e7b69a9118ae5d2c3a
mingjingc <2595400537@qq.com>
2021-04-19 15:28:22 +0800

    feat(proposal):move vapor proposal

--- protocol/consensus.go
+++ protocol/consensus.go
@@ -1,9 +1,17 @@
 package protocol
 
-import "github.com/bytom/bytom/errors"
+import (
+	"crypto/ed25519"
+
+	"github.com/bytom/bytom/errors"
+)
 
 var (
 	// ErrDoubleSignBlock represent the consensus is double sign in same height of different block
 	ErrDoubleSignBlock  = errors.New("the consensus is double sign in same height of different block")
 	errInvalidSignature = errors.New("the signature of block is invalid")
+)
+
+var (
+	SignatureLength = ed25519.SignatureSize
 )
