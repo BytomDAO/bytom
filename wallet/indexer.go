b4b30e0f8451316a176e5832217fc5d442a12adc
hauson <875095736@qq.com>
2021-05-10 17:27:57 +0800

    feat(code format): code format

--- wallet/indexer.go
+++ wallet/indexer.go
@@ -365,3 +365,71 @@ func (w *Wallet) indexBalances(accountUTXOs []*account.UTXO) ([]AccountBalance, 
 
 	return balances, nil
 }
+
+
+// GetAddressBalances return appointed address all assets  balances
+func (w *Wallet) GetAddressBalances(address string) ([]AddressBalance, error) {
+	return w.indexBalancesEx(w.GetAddressUTXOs(address, "", false, false))
+}
+
+type balanceKey struct {
+	Address string
+	Asset   string
+}
+
+// AddressBalance address balance
+type AddressBalance struct {
+	Address         string                 `json:"address"`
+	AssetAlias      string                 `json:"asset_alias"`
+	AssetID         string                 `json:"asset_id"`
+	Amount          uint64                 `json:"amount"`
+	AssetDefinition map[string]interface{} `json:"asset_definition"`
+}
+
+func (w *Wallet) indexBalancesEx(accountUTXOs []*account.UTXO) ([]AddressBalance, error) {
+	address2balance := make(map[*balanceKey]uint64)
+	for _, utxo := range accountUTXOs {
+		key := &balanceKey{Address: utxo.Address, Asset: utxo.AssetID.String()}
+		address2balance[key] += utxo.Amount
+	}
+
+	balances := []AddressBalance{}
+	for _, key := range sortBalanceKeys(address2balance) {
+		targetAsset, err := w.AssetReg.GetAsset(key.Asset)
+		if err != nil {
+			return nil, err
+		}
+
+		assetAlias := *targetAsset.Alias
+		balances = append(balances, AddressBalance{
+			Address:         key.Address,
+			AssetID:         key.Asset,
+			AssetAlias:      assetAlias,
+			Amount:          address2balance[key],
+			AssetDefinition: targetAsset.DefinitionMap,
+		})
+	}
+
+	return balances, nil
+}
+
+func sortBalanceKeys(address2balance map[*balanceKey]uint64) []*balanceKey {
+	var sortedKeys []*balanceKey
+	for key := range address2balance {
+		sortedKeys = append(sortedKeys, key)
+	}
+
+	sort.Slice(sortedKeys, func(i, j int) bool {
+		if sortedKeys[i].Address > sortedKeys[j].Address {
+			return true
+		}
+
+		if (sortedKeys[i].Address == sortedKeys[j].Address) && (sortedKeys[i].Asset > sortedKeys[j].Asset) {
+			return true
+		}
+
+		return false
+	})
+
+	return sortedKeys
+}
