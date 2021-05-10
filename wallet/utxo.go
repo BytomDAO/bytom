f5e49c6439ce9a77679233c4f08e6d4911aaf4f5
hauson <875095736@qq.com>
2021-05-10 17:32:34 +0800

    feat(code format): code format

--- wallet/utxo.go
+++ wallet/utxo.go
@@ -126,6 +126,31 @@ func (w *Wallet) filterAccountUtxo(utxos []*account.UTXO) []*account.UTXO {
 	return result
 }
 
+// GetAddressUTXOs return appointed address all asset  unspent UTXOs(outputs)
+func (w *Wallet) GetAddressUTXOs(address, id string, unconfirmed, isSmartContract bool) []*account.UTXO {
+	prefix := account.UTXOPreFix
+	if isSmartContract {
+		prefix = account.SUTXOPrefix
+	}
+
+	accountUTXOs := []*account.UTXO{}
+	if unconfirmed {
+		accountUTXOs = w.AccountMgr.ListUnconfirmedUTXO(func(utxo *account.UTXO) bool {
+			return segwit.IsP2WScript(utxo.ControlProgram) != isSmartContract && (address == utxo.Address || address == "")
+		})
+	}
+
+	accountUTXOIter := w.DB.IteratorPrefix([]byte(prefix + id))
+	defer accountUTXOIter.Release()
+
+	utxos := getUTXOs(accountUTXOIter, func(utxo *account.UTXO) bool {
+		return address == utxo.Address || address == ""
+	})
+	accountUTXOs = append(accountUTXOs, utxos...)
+
+	return accountUTXOs
+}
+
 func batchSaveUtxos(utxos []*account.UTXO, batch dbm.Batch) error {
 	for _, utxo := range utxos {
 		data, err := json.Marshal(utxo)
