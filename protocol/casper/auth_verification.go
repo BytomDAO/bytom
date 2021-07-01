4633cfa62675659722ceaa98e081db53cb2a2481
Paladz <yzhu101@uottawa.ca>
2021-07-01 14:40:12 +0800

    fix the memory leak (#2003)
    * fix the memory leak
    
    * fix ci
    
    Co-authored-by: paladz <colt@ColtdeMacBook-Pro.local>

--- protocol/casper/auth_verification.go
+++ protocol/casper/auth_verification.go
@@ -122,6 +122,7 @@ func (c *Casper) setFinalized(checkpoint *state.Checkpoint) {
 
 	// update the checkpoint state in memory
 	newRoot.checkpoint.Status = state.Finalized
+	newRoot.checkpoint.Parent = nil
 	c.tree = newRoot
 }
 
