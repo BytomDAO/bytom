a4e1e96ee2b2e308f98b6d15a4651aae9c339d57
hauson <875095736@qq.com>
2021-07-15 14:02:53 +0800

    feat(add genesis tx btm issue amount): add genesis tx btm issue amount

--- config/genesis.go
+++ config/genesis.go
@@ -1,32 +1,12 @@
 package config
 
 import (
-	"encoding/hex"
-
 	log "github.com/sirupsen/logrus"
 
 	"github.com/bytom/bytom/consensus"
 	"github.com/bytom/bytom/protocol/bc"
 	"github.com/bytom/bytom/protocol/bc/types"
 )
-
-func GenesisTx() *types.Tx {
-	contract, err := hex.DecodeString("00148c9d063ff74ee6d9ffa88d83aeb038068366c4c4")
-	if err != nil {
-		log.Panicf("fail on decode genesis tx output control program")
-	}
-
-	txData := types.TxData{
-		Version: 1,
-		Inputs: []*types.TxInput{
-			types.NewCoinbaseInput([]byte("Information is power. -- Jan/11/2013. Computing is power. -- Apr/24/2018.")),
-		},
-		Outputs: []*types.TxOutput{
-			types.NewOriginalTxOutput(*consensus.BTMAssetID, 0, contract, nil),
-		},
-	}
-	return types.NewTx(txData)
-}
 
 func mainNetGenesisBlock() *types.Block {
 	tx := GenesisTx()
