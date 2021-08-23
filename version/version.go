19515f4161d8e554584038754703ddd4a8c658ab
DeKaiju <longjinglv@163.com>
2021-08-23 10:36:06 +0800

    chore(version): change version to 2.0.4

--- version/version.go
+++ version/version.go
@@ -47,7 +47,7 @@ const (
 
 var (
 	// The full version string
-	Version = "2.0.3"
+	Version = "2.0.4"
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
 	Status    *UpdateStatus
