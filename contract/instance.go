47f399640305a9db737b017989e8d26de1c1c393
shenao78 <shenao.78@163.com>
2021-08-31 11:15:03 +0800

    fix calc utxo key

--- contract/instance.go
+++ contract/instance.go
@@ -3,7 +3,9 @@ package contract
 import (
 	"bytes"
 	"encoding/hex"
+	"sort"
 
+	"github.com/bytom/bytom/protocol/bc/types"
 	"golang.org/x/crypto/sha3"
 
 	"github.com/bytom/bytom/protocol/bc"
@@ -46,12 +48,36 @@ func (i *InstanceTable) Put(instance *Instance) {
 }
 
 type UTXO struct {
-	bc.AssetAmount
+	assetID bc.AssetID
+	amount  uint64
 	hash    bc.Hash
 	program []byte
 }
 
+func inputToUTXO(input *types.TxInput) *UTXO {
+	outputID, _ := input.SpentOutputID()
+	return &UTXO{
+		assetID: input.AssetID(),
+		amount:  input.Amount(),
+		hash:    outputID,
+		program: input.ControlProgram(),
+	}
+}
+
+func outputToUTXO(output *types.TxOutput, outputID bc.Hash) *UTXO {
+	return &UTXO{
+		assetID: *output.AssetId,
+		amount:  output.Amount,
+		hash:    outputID,
+		program: output.ControlProgram,
+	}
+}
+
 func utxoKey(utxos []*UTXO) string {
+	sort.Slice(utxos, func(i, j int) bool {
+		return utxos[i].hash.String() < utxos[j].hash.String()
+	})
+
 	buff := new(bytes.Buffer)
 	for _, u := range utxos {
 		buff.Write(u.hash.Bytes())
