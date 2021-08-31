c32a7c00e90efe37b5a62e56dedffdc0c97968ff
shenao78 <shenao.78@163.com>
2021-08-31 16:24:22 +0800

    fix trade service interface

--- contract/trace_service.go
+++ contract/trace_service.go
@@ -7,7 +7,7 @@ import (
 type TraceService interface {
 	CreateNewInstance(txHash, blockHash bc.Hash) (string, error)
 
-	RemoveInstance(traceID string)
+	RemoveInstance(traceID string) error
 
 	GetInstance(traceID string) (*Instance, error)
 }
