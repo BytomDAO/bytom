4786a5980b5f8e4cb6972c28b00e86681a40ecc1
paladz <colt@ColtdeMacBook-Pro.local>
2021-09-14 01:45:28 +0800

    working case for nft contract

---
+++ nft/nft_test.go
@@ -0,0 +1,136 @@
+package nft
+
+import (
+	"testing"
+
+	"github.com/bytom/bytom/consensus"
+	"github.com/bytom/bytom/protocol/bc"
+	"github.com/bytom/bytom/protocol/bc/types"
+	"github.com/bytom/bytom/protocol/validation"
+	"github.com/bytom/bytom/protocol/vm"
+	"github.com/bytom/bytom/testutil"
+)
+
+var (
+	marginFold        = uint64(10)
+	taxRate           = uint64(10)
+	anyCanSpendScript = testutil.MustDecodeHexString("51")
+	platformScript    = []byte("platformScript")
+	createrScript     = []byte("createrScript")
+	nftAsset          = testutil.MustDecodeAsset("a0a71c215764e342d10d003be6369baf4145d9c7977f7b8f6bf446e628d8b8b8")
+	BTC               = testutil.MustDecodeAsset("bda946b3110fa46fd94346ce3f05f0760f1b9de72e238835bc4d19f9d64f1742")
+	ETH               = testutil.MustDecodeAsset("78de44ffa1bce37b757c9eae8925b5f199dc4621b412ef0f3f46168865284a93")
+
+	utxoSourceID = testutil.MustDecodeHash("762ec536ea64f71feac5fd4000a4807fc8e9d08d757889bd0206a02b79f9db8e")
+	ownerScirpt  = []byte("ownerScirpt")
+	buyerScirpt  = []byte("buyerScirpt")
+)
+
+// 从2个BTC的押金换成50个ETH的
+func TestEditMargin(t *testing.T) {
+	contract, err := NewContract(platformScript, marginFold)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	oldStateData := [][]byte{
+		createrScript,
+		vm.Uint64Bytes(taxRate),
+		nftAsset.Bytes(),
+		ownerScirpt,
+		BTC.Bytes(),
+		vm.Uint64Bytes(200000000),
+	}
+
+	newStateData := [][]byte{
+		createrScript,
+		vm.Uint64Bytes(taxRate),
+		nftAsset.Bytes(),
+		ownerScirpt,
+		ETH.Bytes(),
+		vm.Uint64Bytes(50000000000),
+	}
+
+	arguments := [][]byte{
+		ETH.Bytes(),
+		vm.Uint64Bytes(50000000000),
+		vm.Uint64Bytes(1),
+	}
+
+	tx := types.NewTx(types.TxData{
+		Version:        1,
+		SerializedSize: 10000,
+		Inputs: []*types.TxInput{
+			types.NewSpendInput(arguments, utxoSourceID, nftAsset, 1, 0, contract, oldStateData),
+			types.NewSpendInput(arguments, utxoSourceID, BTC, 200000000, 1, contract, oldStateData),
+			types.NewSpendInput(nil, utxoSourceID, ETH, 50000000000, 2, anyCanSpendScript, nil),
+			types.NewSpendInput(nil, utxoSourceID, *consensus.BTMAssetID, 100000000, 1, anyCanSpendScript, nil),
+		},
+		Outputs: []*types.TxOutput{
+			types.NewOriginalTxOutput(nftAsset, 1, contract, newStateData),
+			types.NewOriginalTxOutput(ETH, 50000000000, contract, newStateData),
+			types.NewOriginalTxOutput(BTC, 200000000, ownerScirpt, newStateData),
+		},
+	})
+
+	_, err = validation.ValidateTx(tx.Tx, &bc.Block{BlockHeader: &bc.BlockHeader{}}, func(prog []byte) ([]byte, error) { return nil, nil })
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// 10个ETH质押被120个ETH买走, 然后被质押15个ETH
+func TestBuy(t *testing.T) {
+	contract, err := NewContract(platformScript, marginFold)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	oldStateData := [][]byte{
+		createrScript,
+		vm.Uint64Bytes(taxRate),
+		nftAsset.Bytes(),
+		ownerScirpt,
+		ETH.Bytes(),
+		vm.Uint64Bytes(10000000000),
+	}
+
+	newStateData := [][]byte{
+		createrScript,
+		vm.Uint64Bytes(taxRate),
+		nftAsset.Bytes(),
+		buyerScirpt,
+		ETH.Bytes(),
+		vm.Uint64Bytes(15000000000),
+	}
+
+	arguments := [][]byte{
+		buyerScirpt,
+		vm.Uint64Bytes(120000000000),
+		vm.Uint64Bytes(15000000000),
+		vm.Uint64Bytes(0),
+	}
+
+	tx := types.NewTx(types.TxData{
+		Version:        1,
+		SerializedSize: 10000,
+		Inputs: []*types.TxInput{
+			types.NewSpendInput(arguments, utxoSourceID, nftAsset, 1, 0, contract, oldStateData),
+			types.NewSpendInput(arguments, utxoSourceID, ETH, 10000000000, 1, contract, oldStateData),
+			types.NewSpendInput(nil, utxoSourceID, ETH, 135000000000, 2, anyCanSpendScript, nil),
+			types.NewSpendInput(nil, utxoSourceID, *consensus.BTMAssetID, 100000000, 1, anyCanSpendScript, nil),
+		},
+		Outputs: []*types.TxOutput{
+			types.NewOriginalTxOutput(nftAsset, 1, contract, newStateData),
+			types.NewOriginalTxOutput(ETH, 15000000000, contract, newStateData),
+			types.NewOriginalTxOutput(ETH, 12000000000, createrScript, oldStateData),
+			types.NewOriginalTxOutput(ETH, 1200000000, platformScript, oldStateData),
+			types.NewOriginalTxOutput(ETH, 116800000000, ownerScirpt, oldStateData),
+		},
+	})
+
+	_, err = validation.ValidateTx(tx.Tx, &bc.Block{BlockHeader: &bc.BlockHeader{}}, func(prog []byte) ([]byte, error) { return nil, nil })
+	if err != nil {
+		t.Fatal(err)
+	}
+}
