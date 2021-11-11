09d03841615e152a8773b5fde1f0f2af7b4d1902
LonelyPale <lonelypale@126.com>
2021-11-11 16:27:28 +0800

    update gm

--- accesstoken/accesstoken_gm.go
+++ accesstoken/accesstoken_gm.go
@@ -1,4 +1,4 @@
-// +build !gm
+// +build gm
 
 // Package accesstoken provides storage and validation of Chain Core
 // credentials.
@@ -12,6 +12,7 @@ import (
 	"strings"
 	"time"
 
+	sm3util "github.com/bytom/bytom/crypto/sm3"
 	dbm "github.com/bytom/bytom/database/leveldb"
 	"github.com/bytom/bytom/errors"
 )
@@ -71,9 +72,12 @@ func (cs *CredentialStore) Create(id, typ string) (*Token, error) {
 		return nil, err
 	}
 
+	hashedSecret := make([]byte, tokenSize)
+	sm3util.Sum(hashedSecret, secret)
+
 	token := &Token{
 		ID:      id,
-		Token:   fmt.Sprintf("%s:%x", id, secret),
+		Token:   fmt.Sprintf("%s:%x", id, hashedSecret),
 		Type:    typ,
 		Created: time.Now(),
 	}
--- crypto/sm3/sm3.go
+++ crypto/sm3/sm3.go
@@ -1,9 +1,20 @@
+// +build gm
+
 package sm3
 
-import "github.com/bytom/bytom/crypto"
+import (
+	"github.com/tjfoc/gmsm/sm3"
+)
 
+// Sum256 returns the SM3 digest of the data.
 func Sum256(data []byte) (digest [32]byte) {
-	hash := crypto.Sm3(data)
+	hash := sm3.Sm3Sum(data)
 	copy(digest[:], hash)
 	return
+}
+
+// Sum calculate data into hash
+func Sum(hash, data []byte) {
+	tmp := sm3.Sm3Sum(data)
+	copy(hash, tmp)
 }
